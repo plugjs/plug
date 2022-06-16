75cd2308b94e5fdd24f61826165d95e14182b2f8
Pier Fumagalli <pier@usrz.com>
2022-06-16 23:45:32 +0200

    Async tracking only in logs

--- src/async.ts
+++
@@ -1,8 +0,0 @@
-import { AsyncLocalStorage } from 'node:async_hooks'
-import type { Task } from './build'
-
-export function currentTask(): Task | undefined {
-  return asyncTasksStorage.getStore()
-}
-
-export const asyncTasksStorage = new AsyncLocalStorage<Task>()
