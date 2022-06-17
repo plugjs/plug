225ea93223b122fc90e4fcbce35c2bb1b52c68b7
Pier Fumagalli <pier@usrz.com>
2022-06-17 18:40:00 +0200

    Make `Pipe` extend `Promise<Files>`

--- src/pipe.ts
+++ src/pipe.ts
@@ -1,5 +1,6 @@
 import type { Run } from './run'
 import { Files } from './files'
+import assert from 'node:assert'
 
 export type PlugFunction = (run: Run, files: Files) => Files | Promise<Files>
 
@@ -7,25 +8,36 @@ export interface Plug {
   pipe: PlugFunction
 }
 
-export class Pipe {
-  #plugs: Plug[] = []
-  #run: Run
+export class Pipe extends Promise<Files> {
+  #run!: Run
 
-  constructor(run: Run) {
-    this.#run = run
+  constructor(run: Run, files?: Files)
+  constructor(run: Run | (() => void), files?: Files) {
+    // When subclassing a promise, we _might_ be called with an executor
+    // function (that is, when the promise is `then`-ed). In this case, we
+    // just call `super(...)` with said executor...
+    if (typeof run === 'function') {
+      super(run)
+    } else {
+      super((resolve) => resolve(files || Files.builder(run.directory).build()))
+      this.#run = run
+    }
   }
 
-  async run(): Promise<Files> {
-    let files = Files.builder(this.#run.directory).build()
-    for (const plug of this.#plugs) files = await plug.pipe(this.#run, files)
-    return files
-  }
+  plug(plug: Plug): Pipe
+  plug(plug: PlugFunction): Pipe
+  plug(arg: Plug | PlugFunction): Pipe {
+    assert(this.#run, `Unable to plug components outside of invocation chain`)
 
-  plug(plug: Plug): this
-  plug(plug: PlugFunction): this
-  plug(plug: Plug | PlugFunction): this {
-    if (typeof plug === 'function') plug = { pipe: plug }
-    this.#plugs.push(plug)
-    return this
+    // Normalize our argument as a `Plug` instance
+    const plug = typeof arg === 'function' ? { pipe: arg } : arg
+
+    // We _extend_ a `Promise` so `then(...)` will definitely return `Pipe`,
+    // but that instance won't have our `#run` specified (as it's not passed)
+    // in the constructor. We just manually inject it here, for subsequent
+    // calls...
+    const promise = this.then((files) => plug.pipe(this.#run, files)) as Pipe
+    promise.#run = this.#run
+    return promise
   }
 }
