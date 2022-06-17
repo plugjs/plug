0cd6164a20f700fd6dd6f47f76f89c0bc0c9d47a
Pier Fumagalli <pier@usrz.com>
2022-06-18 00:11:58 +0200

    Docs

--- src/async.ts
+++ src/async.ts
@@ -1,37 +1,28 @@
-import assert from 'node:assert'
 import { AsyncLocalStorage } from 'node:async_hooks'
 import type { Task } from './build'
 import type { Run } from './run'
 
+/* ========================================================================== *
+ * EXPORTED                                                                   *
+ * ========================================================================== */
+
+/**
+ * The {@link AsyncContext} interface describes what needs to be associated
+ * with the current asynchronous invocation context.
+ */
 export interface AsyncContext {
+  /** The {@link Run} to associate with the current invocation context */
   run: Run,
+  /** The {@link Task} to associate with the current invocation context */
   task: Task,
 }
 
-export function currentRun(): Run | undefined {
-  return storage.getStore()?.run
-}
-
-export function currentTask(): Task | undefined {
-  return storage.getStore()?.task
-}
-
-export function requireRun(): Run {
-  const context = storage.getStore()
-  assert(context, 'No `Run` associated with current async context')
-  return context.run
-}
-
-export function requireTask(): Task {
-  const context = storage.getStore()
-  assert(context, 'No `Task` associated with current async context')
-  return context.task
-}
-
-export function runningTasks(): Task[] {
-  return [ ...tasks ]
-}
-
+/**
+ * Run the specified `callback` associating the specified {@link AsyncContext}
+ * with the current asynchronous invocation context.
+ *
+ * @return The {@link Promise} returned by the callback.
+ */
 export function runAsync<T>(context: AsyncContext, callback: () => Promise<T>): Promise<T> {
   return storage.run(context, () => {
     tasks.push(context.task)
@@ -41,6 +32,33 @@ export function runAsync<T>(context: AsyncContext, callback: () => Promise<T>): 
     })
   })
 }
+
+/**
+ * Returns the {@link Run} associated with the current asynchronous invocation
+ * context or `undefined`.
+ */
+export function currentRun(): Run | undefined {
+  return storage.getStore()?.run
+}
+
+/**
+ * Returns the {@link Task} associated with the current asynchronous invocation
+ * context or `undefined`.
+ */
+export function currentTask(): Task | undefined {
+  return storage.getStore()?.task
+}
+
+/**
+ * Return the array of all {@link Task}s currently running
+ */
+export function runningTasks(): Task[] {
+  return [ ...tasks ]
+}
+
+/* ========================================================================== *
+ * INTERNALS                                                                  *
+ * ========================================================================== */
 
 const storage = new AsyncLocalStorage<AsyncContext>()
 const tasks = new Array<Task>()
