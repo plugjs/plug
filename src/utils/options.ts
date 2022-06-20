0654320c4549c9001305da4e540b1f9b8d7061a5
Pier Fumagalli <pier@usrz.com>
2022-06-20 16:13:50 +0200

    Simplify options parsing, by allowing an empty list of strings

--- src/utils/options.ts
+++ src/utils/options.ts
@@ -1,8 +1,6 @@
-type StringArguments = [ string, ...string[] ]
-
-export type ParsedOptions<Options> = { globs: StringArguments, options: Options }
-export type ParsedOptionalOptions<Options> = { globs: StringArguments, options?: Options | undefined }
-export type ParseOptions<Options> = StringArguments | [ ...StringArguments, Options ] | [ Options ]
+export type ParsedOptions<Options> = { params: string[], options: Options }
+export type ParsedOptionalOptions<Options> = { params: string[], options?: Options | undefined }
+export type ParseOptions<Options> = string[] | [ ...string[], Options ] | [ Options ]
 
 /** Parse an array of at least one string, followed by an optional `Options` argument. */
 export function parseOptions<Options>(args: ParseOptions<Options>): ParsedOptionalOptions<Options>
@@ -11,21 +9,14 @@ export function parseOptions<Options>(args: ParseOptions<Options>, defaults?: un
 /** Parse an array of at least one string, followed by an optional `Options` argument, ensuring some defaults are present. */
 export function parseOptions<Options, Defaults extends Options>(args: ParseOptions<Options>, defaults: Defaults): ParsedOptions<Options & Defaults>
 // overloaded implementation
-export function parseOptions<Options, Defaults extends Options>(args: ParseOptions<Options>, defaults?: Defaults): ParsedOptions<Options | Defaults | undefined> {
-  const clone: ParseOptions<Options> = [ ...args ]
-  const last = clone.pop()
+export function parseOptions<Options, Defaults extends Options>(args: ParseOptions<Options>, defaults?: Defaults): ParsedOptions<any> {
+  const params: string[] = []
+  const options: any = { ...defaults }
 
-  const { globs, options } =
-    typeof last === 'string' ? {
-      globs: [ ...clone, last ] as StringArguments,
-      options: defaults,
-    } : defaults ? {
-      globs: [ ...clone ] as StringArguments,
-      options: { ...defaults, ...last },
-    } : {
-      globs: [ ...clone ] as StringArguments,
-      options: last,
-    }
+  for (const arg of args) {
+    if (typeof arg === 'string') params.push(arg)
+    else Object.assign(options, arg)
+  }
 
-  return { globs, options }
+  return { params, options }
 }
