fec6aa62333dce857907280b1ee9c177d218f4a0
Pier Fumagalli <pier@usrz.com>
2022-06-21 11:57:58 +0200

    Finalize (somehow) `esbuild` plug

--- src/plugs/esbuild.ts
+++ src/plugs/esbuild.ts
@@ -1,42 +1,68 @@
-import { Files, FilesBuilder } from '../files'
-import type { Plug } from '../pipe'
-import type { Run } from '../run'
-import * as esb from 'esbuild'
 import assert from 'assert'
 import path from 'path'
+
 import { $p, log } from '../log'
+import { build, BuildOptions } from 'esbuild'
+import { Files, FilesBuilder } from '../files'
+
+import type { Plug } from '../pipe'
+import type { Run } from '../run'
+
+export type ESBuildOptions = Omit<BuildOptions, 'absWorkingDir' | 'entryPoints' | 'watch'>
 
 export class ESBuild implements Plug {
-  #options: esb.BuildOptions
+  #options: ESBuildOptions
 
-  constructor(options: esb.BuildOptions) {
+  constructor(options: ESBuildOptions) {
     this.#options = options
   }
 
-  async pipe(run: Run, files: Files): Promise<Files> {
-    const options = { ...this.#options }
+  async pipe(run: Run, _files: Files): Promise<Files> {
+    const entryPoints = [ ..._files.absolutePaths() ]
+    const absWorkingDir = _files.directory
 
-    options.absWorkingDir = files.directory
-    options.entryPoints = [ ...files ]
-    options.target = 'node16' // TODO
-    options.format = 'cjs' // TODO
+    const options: BuildOptions = {
+      /* Defaults */
+      platform: 'node',
+      target: `node${process.versions['node']}`,
+      format: 'cjs',
 
-    let builder: FilesBuilder
-    if (options.bundle) {
-      throw new Error('Not yet bundling')
-    } else {
-      assert(options.outdir, 'Option "outdir" must be specified when not bundling')
-      builder = Files.builder(run, options.outdir)
-      options.outdir = builder.directory
-      log.debug('Transpiling', files.length, 'files to', $p(builder.directory))
+      /* Our options */
+      ...this.#options,
+
+      /* Always override */
+      absWorkingDir,
+      entryPoints,
     }
 
-    const { metafile } = await esb.build({ ...options, metafile: true })
+    /* Sanity check on output file/directory */
+    assert(!(options.outdir && options.outfile), 'Options "outfile" and "outdir" can not coexist')
+    assert(!(options.watch), 'Option "watch" can not work in plugs')
+
+    /* Where to write, where to write? */
+    let builder: FilesBuilder
+    if (options.bundle && options.outfile && (entryPoints.length === 1)) {
+
+      builder = Files.builder(run, absWorkingDir)
+      options.outfile = path.resolve(absWorkingDir, options.outfile)
+
+      log.debug('Bundling', $p(entryPoints[0]), 'into', $p(options.outfile))
+    } else {
+      assert(options.outdir, 'Option "outdir" must be specified')
+
+      builder = Files.builder(run, options.outdir)
+      options.outdir = builder.directory
+
+      const message = options.bundle ? 'Bundling' : 'Transpiling'
+      log.debug(message, entryPoints.length, 'files to', $p(builder.directory))
+    }
+
+    const { metafile } = await build({ ...options, metafile: true })
     const outputs = metafile.outputs
 
     for (const file in outputs) {
-      const source = path.resolve(files.directory, outputs[file].entryPoint!)
-      const target = path.resolve(files.directory, file)
+      const source = path.resolve(absWorkingDir, outputs[file].entryPoint!)
+      const target = path.resolve(absWorkingDir, file)
       log.trace('Transpiled', $p(source), 'to', $p(target))
       builder.add(target)
     }
@@ -44,7 +70,9 @@ export class ESBuild implements Plug {
     const result = builder.build()
     log.info('ESBuild produced', result.length, 'files into', $p(result.directory))
     return result
-
-    // throw new Error('Method not implemented.')
   }
+}
+
+export function esbuild(options: ESBuildOptions) {
+  return new ESBuild(options)
 }
