1afb2f052bcf6a0bc729a0228b03d0e5f5f1ed9f
Pier Fumagalli <pier@usrz.com>
2022-06-22 01:23:17 +0200

    Debug files plug

---
+++ src/plugs/debug.ts
@@ -0,0 +1,26 @@
+import { Files } from '../files'
+import { $p, log } from '../log'
+import { Plug } from '../pipe'
+import { Run } from '../run'
+
+export class Debug implements Plug {
+  constructor() {
+    // nothing to do
+  }
+  pipe(run: Run, files: Files): Files | Promise<Files> {
+
+    log.info('Debugging', files.length, 'files')
+    log.info('-   run directory:', $p(run.directory))
+    log.info('- files directory:', $p(files.directory))
+    if (files.length) {
+      const [ path, ...paths ] = [ ...files ]
+      log.info('-           files:', $p(path))
+      for (const p of paths) log.info('-                :', $p(p))
+    }
+    return files
+  }
+}
+
+export function debug() {
+  return new Debug()
+}
