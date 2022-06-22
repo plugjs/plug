1b21423b6bd8a661d09147141080c1c2373314f7
Pier Fumagalli <pier@usrz.com>
2022-06-22 20:34:09 +0200

    Better test names and console output

--- test/00-test.ts
+++ test/00-test.ts
@@ -1,22 +1,22 @@
 import { describe, it } from '../src/test'
 
-describe('X) Basic test', () => {
-  it('1) Test number one', () => {
-    //console.log('> Test number one')
+describe('Basic test', () => {
+  it('Test number one', () => {
+    console.log('Test number one')
   })
 
-  describe('2) Test number two', () => {
-    it('2A) Test 2A', () => {})
-    it.skip('2B) Test 2B', () => {})
-    it('2C) Test 2C', () => {})
-    it('2D) Test 2D', () => {})
+  describe('Test number two', () => {
+    it('Test 2A', () => {})
+    it.skip('Test 2B', () => {})
+    it('Test 2C', () => {})
+    it('Test 2D', () => {})
   })
 
-  it('3) This test fails', () => {
+  it('This test fails', () => {
     throw new Error('FooBar')
   })
 
-  it('4) Test number three', () => {
-    // console.error('This goes to standard error')
+  it('Test number three', () => {
+    console.error('This goes to standard error')
   })
 })
