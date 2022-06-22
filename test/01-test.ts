9a6d68e98d069564a56d10254aaf99cf1ab77852
Pier Fumagalli <pier@usrz.com>
2022-06-22 18:28:54 +0200

    Second test file

---
+++ test/01-test.ts
@@ -0,0 +1,9 @@
+import { describe, it } from '../src/test'
+
+describe('Another suite', () => {
+  it('Another nested test', () => {
+    //console.log('> Test number one')
+  })
+})
+
+it('This is a root test', () => {})
