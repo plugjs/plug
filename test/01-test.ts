31652637f331ca5c8e2708b2ff04f643ba286dd4
Pier Fumagalli <pier@usrz.com>
2022-07-19 21:03:07 +0200

    Wipe *our* test runner

--- test/01-test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, it } from '../src/test'
-
-describe('Another suite', () => {
-  it('Another nested test', () => {
-    // console.log('Another message for the console')
-  })
-})
-
-it('This is a root test', () => {})
