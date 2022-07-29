4f9815c68afd20b8f9eb8cc9e9414a0a582e4d7d
Pier Fumagalli <pier@usrz.com>
2022-07-29 20:27:08 +0200

    Simplify `Plug`/`Pipe` contract

--- src/pipe.ts
+++ src/pipe.ts
@@ -1,23 +1,37 @@
-import { Files } from './files'
-import { Plug, PlugContext, PlugFunction } from './plug'
+import type { Files } from './files'
+import type { Run } from './run'
+
+export interface Plug {
+  pipe(files: Files, run: Run): Files | Promise<Files>
+}
+
+export type PlugFunction = Plug['pipe']
 
 export class Pipe {
-  private readonly _plugs: Plug[] = []
+  readonly #plugs: Plug[] = [] // esplicitly hidden to allow pipes extension
+  readonly #start: (run: Run) => Pipe | Files | Promise<Files>
 
-  constructor(
-    private readonly _start: () => Files | Promise<Files>,
-  ) {}
+  constructor(start: ((run: Run) => Pipe | Files | Promise<Files>)) {
+    if (typeof start === 'function') {
+      this.#start = start
+    } else {
+      this.#start = (run: Run) => Pipe.run(start, run)
+    }
+  }
 
   plug(plug: Plug): this
   plug(plug: PlugFunction): this
   plug(plug: Plug | PlugFunction): this {
-    this._plugs.push(typeof plug === 'function' ? { pipe: plug } : plug)
+    this.#plugs.push(typeof plug === 'function' ? { pipe: plug } : plug)
     return this
   }
 
-  run(context: PlugContext): Promise<Files> {
-    return this._plugs.reduce((prev, plug) => {
-      return prev.then((curr) => plug.pipe(curr, context))
-    }, Promise.resolve().then(() => this._start()))
+  static run(pipe: Pipe, run: Run): Promise<Files> {
+    return pipe.#plugs.reduce((prev, plug) => {
+      return prev.then((curr) => plug.pipe(curr, run))
+    }, Promise.resolve().then(async () => {
+      const result = await pipe.#start(run)
+      return 'plug' in result ? Pipe.run(result, run) : result
+    }))
   }
 }
