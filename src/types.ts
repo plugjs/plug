320a3b0baa9f7809eebd044dc8e7cb8c5afc9a4f
Pier Fumagalli <pier@usrz.com>
2022-07-29 20:32:21 +0200

    Found a home for `FindOptions`

--- src/types.ts
+++ src/types.ts
@@ -2,21 +2,6 @@
  * TYPES OVERRIDES / EXTENSIONS                                               *
  * ========================================================================== */
 
-import { WalkOptions } from './utils/walk'
-
-/**
- * The {@link FindOptions} interface defines the options available to
- * {@link TaskContext.find}.
- */
-export interface FindOptions extends WalkOptions {
-  /**
-   * The directory where to start looking for files.
-   *
-   * @defaultValue The current {@link Run.directory}
-   */
-   directory?: string
-}
-
 /*
  * Type definition for `WebAssembly`. This is normally provided to TypeScript
  * by `lib.dom.d.ts`, and is not defined by Node's own types.
