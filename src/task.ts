8117a24a91039089ce01f4827d18677b9baa6d68
Pier Fumagalli <pier@usrz.com>
2022-08-02 01:02:03 +0200

    Tasks can return void

--- src/task.ts
+++ src/task.ts
@@ -6,15 +6,15 @@ import type { Run } from './run'
  * TASK                                                                       *
  * ========================================================================== */
 
-export interface Task {
+export interface Task<T extends Files | void = Files | void> {
   /** The {@link BuildContext} of where this task was originally defined */
   readonly context: BuildContext
 
   /** Invoked by the {@link Run} when actually executing this {@link Task} */
-  call(thisBuild: ThisBuild<any>, run: Run): Promise<Files | void>
+  call(thisBuild: ThisBuild<any>, run: Run): Promise<T>
 }
 
-export class TaskImpl implements Task {
+export class TaskImpl implements Task<Files | void> {
   constructor(
       readonly context: BuildContext,
       private readonly _definition: TaskDefinition<any>,
