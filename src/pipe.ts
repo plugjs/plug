1c7e334639eb7714289f200558c1acbd7650bfc5
Pier Fumagalli <pier@usrz.com>
2022-08-01 16:17:44 +0200

    Allow direct installation of plugs into pipes

--- src/pipe.ts
+++ src/pipe.ts
@@ -1,17 +1,31 @@
 import type { Files } from './files'
 import type { Run } from './run'
 
+/**
+ * The {@link Plug} interface describes an extension mechanism for our build.
+ */
 export interface Plug {
   pipe(files: Files, run: Run): Files | void | Promise<Files | void>
 }
 
+/**
+ * A type identifying a {@link Plug} as a `function`
+ */
 export type PlugFunction = Plug['pipe']
 
+/**
+ * A {@link Pipe} represents a sequence of operations performed by {@link Plug}s
+ */
 export interface Pipe extends Promise<Files | void> {
+  /** Add a new {@link Plug} to the steps of this {@link Pipe} */
   plug(plug: Plug): this
+  /** Add a new {@link Plug} to the steps of this {@link Pipe} */
   plug(plug: PlugFunction): this
 }
 
+/**
+ * Implementation of our {@link Pipe} interface.
+ */
 export class Pipe implements Pipe {
   #promise: Promise<Files | void>
   readonly #run: Run
@@ -50,4 +64,55 @@ export class Pipe implements Pipe {
   ): Promise<Files | void> {
     return this.#promise.finally(onfinally)
   }
+}
+
+/* ========================================================================== *
+ * PLUG INSTALLATION (INTERNAL)                                               *
+ * ========================================================================== */
+
+/** The names which can be installed as direct plugs. */
+export type PlugNames = string & Exclude<keyof Pipe, 'plug' | keyof Promise<Files | void>>
+
+/**
+ * Install a {@link Plug} into our {@link Pipe} prototype.
+ *
+ * This allows our shorthand syntax for well-defined plugs such as:
+ *
+ * ```
+ * find('./src', '*.ts').write('./target')
+ * // Nicer and easier than...
+ * find('./src', '*.ts').plug(new Write('./target'))
+ * ```
+ *
+ * Use this alongside interface merging like:
+ *
+ * ```
+ * declare module '../pipe' {
+ *   export interface Pipe {
+ *     write(...args: ConstructorParameters<typeof Write>): this
+ *   }
+ * }
+ * ```
+ */
+export function install<PlugName extends PlugNames, PlugExtension extends Plug>(
+    name: PlugName,
+    ctor: new (...args: Parameters<Pipe[PlugName]>) => PlugExtension,
+): (...args: Parameters<Pipe[PlugName]>) => PlugExtension {
+  /* Create the function to instantiate the Plug */
+  const creator = function(...args: Parameters<Pipe[PlugName]>): PlugExtension {
+    // eslint-disable-next-line new-cap
+    return new ctor(...args)
+  }
+
+  /* Inject the creator within the Pipe prototype */
+  Pipe.prototype[name] = function(...args: Parameters<Pipe[PlugName]>): Pipe {
+    return this.plug(creator(...args))
+  }
+
+  /* Setup names so that stack traces look better */
+  Object.defineProperty(creator, 'name', { value: name })
+  Object.defineProperty(Pipe.prototype[name], 'name', { value: name })
+
+  /* Return our creator */
+  return creator
 }
