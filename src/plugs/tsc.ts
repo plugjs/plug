fc731dadcb300d1f76ceb5505dade50f40d18f5d
Pier Fumagalli <pier@usrz.com>
2022-08-03 01:14:33 +0200

    TypeScript compilation

--- src/plugs/tsc.ts
+++ src/plugs/tsc.ts
@@ -1,56 +1,91 @@
-import { createProgram, getPreEmitDiagnostics } from 'typescript'
 import type { CompilerOptions } from 'typescript'
+import { createProgram, getPreEmitDiagnostics } from 'typescript'
+import { assertSettled } from '../assert'
 import { Files } from '../files'
-import { log } from '../log'
+import { $p, log } from '../log'
+import { isFile } from '../paths'
+import { install, Plug } from '../pipe'
+import { Run } from '../run'
+import { ParseOptions, parseOptions } from '../utils/options'
 import { TypeScriptHost } from './tsc/compiler'
 import { getCompilerOptions } from './tsc/options'
-import { Plug } from '../pipe'
-import { Run } from '../run'
 
 
-export class Compile implements Plug {
-  #options?: CompilerOptions
-  #config?: string
+export class Tsc implements Plug {
+  private readonly _tsconfig?: string
+  private readonly _options: CompilerOptions
 
-  /** ConstructorDoc w/ options */
-  constructor(options?: CompilerOptions)
-  /** ConstructorDoc w/ config */
-  constructor(config?: string, options?: CompilerOptions)
+  constructor()
+  constructor(config: string)
+  constructor(options: CompilerOptions)
+  constructor(config: string, options: CompilerOptions)
 
-  constructor(first: string | CompilerOptions | undefined, extra?: CompilerOptions) {
-    const { config, options } =
-      typeof first === 'string' ? { config: first, options: extra } :
-      first === undefined ? { config: undefined, options: extra } :
-      { config: undefined, options: first }
-
-    this.#options = options
-    this.#config = config
+  constructor(...args: ParseOptions<CompilerOptions>) {
+    const { params: [ tsconfig ], options } = parseOptions(args, {})
+    this._tsconfig = tsconfig
+    this._options = options
   }
 
   async pipe(files: Files, run: Run): Promise<Files> {
-    const host = new TypeScriptHost(run.resolve('.'), run.log)
+    const tsconfig =
+      this._tsconfig ? run.resolve(this._tsconfig) :
+        await isFile(files.directory, 'tsconfig.json')
 
+    const overrides: CompilerOptions = {
+      rootDir: files.directory, // by default, our "files" directory
+      ...this._options, // any other options specified in the constructor
+    }
 
+    const { errors, options } = await getCompilerOptions(
+        tsconfig, // resolved tsconfig.json from constructor, might be undefined
+        overrides, // overrides from constructor, might be an empty object
+        run.buildFile) // overrides are defined in the build file, sooo.....
 
-    const { options, errors } = await getCompilerOptions()
+    const { rootDir, outDir } = options
+    const root = rootDir ? run.resolve(rootDir) : files.directory
+    const out = outDir ? run.resolve(outDir) : root
+
+    const host = new TypeScriptHost(root, run.log)
 
     const paths = [ ...files.absolutePaths() ]
     for (const path of paths) log.trace(`Compiling "${path}"`)
 
     // Get our build file and create the master program
     log.info('Compiling', paths.length, 'files')
+    log.info('Compliation options', options)
 
     const program = createProgram(paths, options, host, undefined, errors)
     const diagnostics = getPreEmitDiagnostics(program)
     host.checkDiagnostics(diagnostics)
 
+    const builder = run.files(out)
+    const promises: Promise<void>[] = []
     const result = program.emit(undefined, (fileName, code) => {
-      void code // TODO!
-      log.info('Should write', fileName)
+      promises.push(builder.write(fileName, code).then((file) => {
+        log.info('Written', $p(file))
+      }))
     })
 
     // Check for errors...
     host.checkDiagnostics(result.diagnostics)
-    return run.files().build()
+
+    const settlements = await Promise.allSettled(promises)
+    assertSettled(settlements, 'Unable to write')
+
+    const outputs = builder.build()
+    log.info('TSC produced', outputs.length, 'files into', $p(outputs.directory))
+    return outputs
+  }
+}
+
+/* ========================================================================== *
+ * INSTALLATION                                                               *
+ * ========================================================================== */
+
+install('tsc', Tsc)
+
+declare module '../pipe' {
+  export interface Pipe {
+    tsc: PipeExtension<typeof Tsc>
   }
 }
