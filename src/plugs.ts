0c29383b3afeb2c4af6832c422bb96c606904af6
Pier Fumagalli <pier@usrz.com>
2022-08-02 23:09:40 +0200

    Group all plugs exports

---
+++ src/plugs.ts
@@ -0,0 +1,6 @@
+export * from './plugs/copy'
+export * from './plugs/coverage'
+export * from './plugs/debug'
+export * from './plugs/esbuild'
+export * from './plugs/exec'
+export * from './plugs/filter'
