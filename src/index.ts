206f4667fca8f6c60883055bef13aa672dd19000
Pier Fumagalli <pier@usrz.com>
2022-08-02 23:09:56 +0200

    Module exports

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+export * from './build'
+export * from './plugs'
+export * from './log'
+export * from './helpers'
+
+// Utility types
+export type { MatchOptions, MatchResult } from './utils/match'
+export type { ParseOptions } from './utils/options'
+export type { WalkOptions } from './utils/walk'
+
+// PlugJS types
+export type { AbsolutePath } from './paths'
+export type { Files, FilesBuilder } from './files'
+export type { FindOptions, Run } from './run'
+export type { Pipe, Plug, PlugFunction } from './pipe'
+export type { Task } from './task'
