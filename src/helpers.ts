dc9599964ed817678f836a5f5711307571c8d561
Pier Fumagalli <pier@usrz.com>
2022-08-03 17:51:38 +0200

    Cleanup

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,20 +1,11 @@
+import type { Files, FilesBuilder } from './files'
 import type { AbsolutePath } from './paths'
-import type { Files } from './files'
+import type { Pipe } from './pipe'
 import type { FindOptions } from './run'
 import type { ParseOptions } from './utils/options'
 
-import { Pipe } from './pipe'
-import { assert, assertSettled } from './assert'
+import { assert, fail } from './assert'
 import { currentRun } from './async'
-
-/**
- * Find files according to the globs and {@link FindOptions} specified.
- */
-export function find(glob: string, ...args: ParseOptions<FindOptions>): Pipe {
-  const run = currentRun()
-  assert(run, 'Unable to find files outside a running task')
-  return run.find(glob, ...args)
-}
 
 /**
  * Resolve a path into an {@link AbsolutePath}.
@@ -29,21 +20,54 @@ export function resolve(...paths: string[]): AbsolutePath {
   return run.resolve(...paths)
 }
 
+/**
+ * Create a new {@link Files} instance.
+ */
+export function files(...paths: string[]): FilesBuilder {
+  const run = currentRun()
+  assert(run, 'Unable to create files builder outside a running task')
+  return run.files(...paths)
+}
+
+/**
+ * Find files according to the globs and {@link FindOptions} specified.
+ */
+export function find(glob: string, ...args: ParseOptions<FindOptions>): Pipe & Promise<Files> {
+  const run = currentRun()
+  assert(run, 'Unable to find files outside a running task')
+  return run.find(glob, ...args)
+}
+
 /** Create a {@link Pipe} from a {@link Files} instance. */
-export function pipe(files: Files | Promise<Files>): Pipe {
+export function pipe(files: Files | Promise<Files>): Pipe & Promise<Files> {
   const run = currentRun()
   assert(run, 'Unable to create pipes outside a running task')
-  return new Pipe(Promise.resolve().then(() => files), run)
+  return run.pipe(files)
 }
 
 /** Await for the settlement of all the promises, then return their results. */
 export async function parallel<P extends readonly any[]>(...promises: P): Promise<ParallelResult<P>> {
   const settlements = await Promise.allSettled(promises)
-  assertSettled(settlements, 'Parallel execution failed')
-  return settlements.map((r) => {
-    assert(r.status === 'fulfilled', 'Promise not fullfilled') // WHY OH WHY???
-    return r.value
-  }) as ParallelResult<P>
+  const results: any[] = []
+
+  let errors = 0
+  for (const settlement of settlements) {
+    if (settlement.status === 'fulfilled') {
+      results.push(settlement.value)
+      continue
+    }
+
+    try {
+      fail(settlement.reason)
+    } catch (error) {
+      // we'll fail below, regardless
+    } finally {
+      errors ++
+    }
+  }
+
+  if (errors) fail('Parallel execution failed for', errors, 'tasks')
+  return results as ParallelResult<P>
 }
 
 type ParallelResult<T extends readonly any[]> =
