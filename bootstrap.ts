d12a6ec1b04a3c113da0ef3cc74bcd7da0e17cf5
Pier Fumagalli <pier@usrz.com>
2022-08-03 22:50:50 +0200

    Bootstrap CJS and MJS

--- bootstrap.ts
+++ bootstrap.ts
@@ -1,34 +1,52 @@
-import { build, log, find, parallel } from './src/index'
+import { build, log, find, parallel, pipe, files, fixExtensions } from './src/index'
+
 
 const booststrap = build({
   async compile_sources() {
-    return find('**/*.ts', { directory: 'src' })
-        .esbuild({ outdir: 'build/src' })
+    const sources = await find('**/*.ts', { directory: 'src' })
+
+    const cjs = await pipe(sources)
+        .esbuild({
+          outdir: 'dist',
+          format: 'cjs',
+          plugins: [ fixExtensions ],
+        })
+
+    const esm = await pipe(sources)
+        .esbuild({
+          outdir: 'dist',
+          format: 'esm',
+          plugins: [ fixExtensions ],
+          outExtension: { '.js': '.mjs' },
+        })
+
+    return files('dist').merge(cjs, esm).build()
   },
+
   async compile_tests() {
     return find('**/*.ts', { directory: 'test' })
         .esbuild({ outdir: 'build/test' })
   },
+
   async compile_types() {
     return find('**/*.ts', { directory: 'src' })
-        .debug()
         .tsc('tsconfig.json', {
           noEmit: false,
           declaration: true,
           emitDeclarationOnly: true,
-          outDir: './build/types',
+          outDir: './types',
         })
-        .debug()
   },
+
   async default() {
+    await this.compile_types()
     await parallel(
         this.compile_sources(),
         this.compile_tests(),
-        this.compile_types(),
     )
   },
 })
 
 booststrap.default()
-    .then((result) => log.info('All done!', result))
+    .then(() => log.info('All done!'))
     .catch((error) => log.error('Build error', error))
