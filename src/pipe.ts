cd2d659abaed1ab07a1da4cf382e19de2d6db53c
Pier Fumagalli <pier@usrz.com>
2022-08-03 17:12:04 +0200

    Allow `Plug`s to return `void` and interrupt pipes

--- src/pipe.ts
+++ src/pipe.ts
@@ -1,66 +1,74 @@
 import type { Files } from './files'
 import type { Run } from './run'
-import type { ConstructorArguments } from './types'
+
+import { assert } from './assert'
 
 /**
  * The {@link Plug} interface describes an extension mechanism for our build.
  */
-export interface Plug {
-  pipe(files: Files, run: Run): Files | Promise<Files>
+export interface Plug<T extends Files | void> {
+  pipe(files: Files, run: Run): T | Promise<T>
 }
 
 /**
  * A type identifying a {@link Plug} as a `function`
  */
-export type PlugFunction = Plug['pipe']
+export type PlugFunction<T extends Files | void> = Plug<T>['pipe']
 
 /**
  * A {@link Pipe} represents a sequence of operations performed by
  * a series of {@link Plug | Plugs}.
  */
-export interface Pipe extends Promise<Files> {
-  /** Add a new {@link Plug} to the steps of this {@link Pipe} */
-  plug(plug: Plug): Pipe
-  /** Add a new {@link Plug} to the steps of this {@link Pipe} */
-  plug(plug: PlugFunction): Pipe
+export interface Pipe {
+  /* Left empty, for definitions of installed plugs as extensions */
 }
 
 /**
- * Implementation of our {@link Pipe} interface.
+ * The {@link Pipe} abstract class exposes the prototype upon which all
+ * extension plugs will be installed on.
  */
-export class Pipe implements Pipe {
-  readonly #promise: Promise<Files>
+export abstract class Pipe implements Pipe {
+  abstract plug(plug: Plug<Files> | PlugFunction<Files>): Pipe & Promise<Files>
+  abstract plug(plug: Plug<void> | PlugFunction<void>): Promise<void>
+}
+
+
+/** Implementation of our {@link Pipe}. */
+export class PipeImpl<T extends Files | void> extends Pipe implements Promise<T> {
+  readonly #promise: Promise<T>
   readonly #run: Run
 
-  constructor(start: Promise<Files>, run: Run) {
-    this.#promise = start
+  constructor(start: T | Promise<T>, run: Run) {
+    super()
+    this.#promise = Promise.resolve(start)
     this.#run = run
   }
 
-  plug(plug: Plug): Pipe
-  plug(plug: PlugFunction): Pipe
-  plug(arg: Plug | PlugFunction): Pipe {
+  plug<T extends Files | void>(arg: Plug<T> | PlugFunction<T>): Pipe & Promise<T> {
     const plug = typeof arg === 'function' ? { pipe: arg } : arg
-    const start = this.#promise.then((files) => plug.pipe(files, this.#run))
-    return new Pipe(start, this.#run)
+    const promise = this.#promise.then((files) => {
+      assert(files, 'Pipe can not be further extended')
+      return plug.pipe(files, this.#run)
+    })
+    return new PipeImpl(promise, this.#run)
   }
 
-  then<T1 = Files, T2 = never>(
-      onfulfilled?: ((value: Files) => T1 | PromiseLike<T1>) | null | undefined,
+  then<T1 = T, T2 = never>(
+      onfulfilled?: ((value: T) => T1 | PromiseLike<T1>) | null | undefined,
       onrejected?: ((reason: any) => T2 | PromiseLike<T2>) | null | undefined,
   ): Promise<T1 | T2> {
     return this.#promise.then(onfulfilled, onrejected)
   }
 
-  catch<T = never>(
-      onrejected?: ((reason: any) => T | PromiseLike<T>) | null | undefined,
-  ): Promise<T | Files> {
+  catch<T0 = never>(
+      onrejected?: ((reason: any) => T0 | PromiseLike<T0>) | null | undefined,
+  ): Promise<T0 | T> {
     return this.#promise.catch(onrejected)
   }
 
   finally(
       onfinally?: (() => void) | null | undefined,
-  ): Promise<Files> {
+  ): Promise<T> {
     return this.#promise.finally(onfinally)
   }
 
@@ -72,7 +80,87 @@ export class Pipe implements Pipe {
  * ========================================================================== */
 
 /** The names which can be installed as direct plugs. */
-export type PlugName = string & Exclude<keyof Pipe, 'plug' | keyof Promise<Files>>
+type PlugName = string & Exclude<keyof Pipe, 'plug' | keyof Promise<Files>>
+
+/** A convenience type identifying a {@link Plug} constructor. */
+type PlugConstructor = new (...args: any) => Plug<Files | void>
+
+/** Convert the resulting type of a {@link Plug} for use in a {@link Pipe} */
+type PlugReturnForPipe<T> =
+  T extends Plug<infer R> ?
+    R extends Files ?
+      Promise<Files> & Pipe :
+    R extends void ?
+      Promise<void> :
+    never :
+  never
+
+/**
+ * Map constructors into an array of all known overloads.
+ *
+ * This is a _royal_ pain in the ass, as we need to distinguish between
+ * all possible number of overloads of a constructor... Limit to 5 of them!
+ *
+ * Also, the empty constructor (when specified in the overloads) will simply
+ * match the first case (most overloads) and generate functions somewhat like
+ *
+ * (...args: unknown[]) => never
+ * (...args: unknown[]) => never
+ * (...args: unknown[]) => never
+ * () => ReturnFilesOrVoid<R3>
+ * (arg: Options) => ReturnFilesOrVoid<R4>
+ *
+ * Somehow inferring the result to `Function` or the right type and ANDing all
+ * those together here doesn't work, so we create this array and we'll AND
+ * all its members in the PipeExtension<...> type.
+ */
+type PlugConstructorOverloads<T extends PlugConstructor> =
+  T extends {
+    new (...args: infer A0): infer R0
+    new (...args: infer A1): infer R1
+    new (...args: infer A2): infer R2
+    new (...args: infer A3): infer R3
+    new (...args: infer A4): infer R4
+  } ? [
+    R0 extends Plug<Files | void> ? ((...args: A0) => PlugReturnForPipe<R0>) : Function,
+    R1 extends Plug<Files | void> ? ((...args: A1) => PlugReturnForPipe<R1>) : Function,
+    R2 extends Plug<Files | void> ? ((...args: A2) => PlugReturnForPipe<R2>) : Function,
+    R3 extends Plug<Files | void> ? ((...args: A3) => PlugReturnForPipe<R3>) : Function,
+    R4 extends Plug<Files | void> ? ((...args: A4) => PlugReturnForPipe<R4>) : Function,
+  ] :
+  T extends {
+    new (...args: infer A0): infer R0
+    new (...args: infer A1): infer R1
+    new (...args: infer A2): infer R2
+    new (...args: infer A3): infer R3
+  } ? [
+    R0 extends Plug<Files | void> ? (...args: A0) => PlugReturnForPipe<R0> : Function,
+    R1 extends Plug<Files | void> ? (...args: A1) => PlugReturnForPipe<R1> : Function,
+    R2 extends Plug<Files | void> ? (...args: A2) => PlugReturnForPipe<R2> : Function,
+    R3 extends Plug<Files | void> ? (...args: A3) => PlugReturnForPipe<R3> : Function,
+  ] :
+  T extends {
+    new (...args: infer A0): infer R0
+    new (...args: infer A1): infer R1
+    new (...args: infer A2): infer R2
+  } ? [
+    R0 extends Plug<Files | void> ? (...args: A0) => PlugReturnForPipe<R0> : Function,
+    R1 extends Plug<Files | void> ? (...args: A1) => PlugReturnForPipe<R1> : Function,
+    R2 extends Plug<Files | void> ? (...args: A2) => PlugReturnForPipe<R2> : Function,
+  ] :
+  T extends {
+    new (...args: infer A0): infer R0
+    new (...args: infer A1): infer R1
+  } ? [
+    R0 extends Plug<Files | void> ? (...args: A0) => PlugReturnForPipe<R0> : Function,
+    R1 extends Plug<Files | void> ? (...args: A1) => PlugReturnForPipe<R1> : Function,
+  ] :
+  T extends {
+    new (...args: infer A0): infer R0
+  } ? [
+    R0 extends Plug<Files | void> ? (...args: A0) => PlugReturnForPipe<R0> : Function,
+  ] :
+  never
 
 /**
  * A convenience type to easily annotate installed {@link Plug Plugs}.
@@ -93,8 +181,12 @@ export type PlugName = string & Exclude<keyof Pipe, 'plug' | keyof Promise<Files
  * }
  * ```
  */
-export type PipeExtension<T extends new (...args: any) => Plug> =
-  (...args: ConstructorArguments<T>) => Pipe
+export type PipeExtension<T extends PlugConstructor, A = PlugConstructorOverloads<T>> =
+  A extends readonly [ infer First, ...infer Rest ] ?
+    First & PipeExtension<T, Rest> :
+  A extends readonly [ infer Only ] ?
+    Only :
+  Function
 
 /**
  * Install a {@link Plug} into our {@link Pipe} prototype, and return a static
@@ -124,16 +216,17 @@ export type PipeExtension<T extends new (...args: any) => Plug> =
  * }
  * ```
  */
-export function install<C extends new (...args: any) => Plug>(
-    name: PlugName,
-    ctor: C,
-): void {
-  /* Inject the creator within the Pipe prototype */
-  Pipe.prototype[name] = function(this: Pipe, ...args: any): Pipe {
+export function install<C extends PlugConstructor>(name: PlugName, ctor: C): void {
+  /* This is quite hairy when it comes to types, so, just give up! :-P */
+
+  function create(this: Pipe, ...args: any): Pipe & Promise<Files | void> {
     // eslint-disable-next-line new-cap
-    return this.plug(new ctor(...args))
+    return this.plug(new ctor(...args) as any)
   }
 
   /* Setup name so that stack traces look better */
-  Object.defineProperty(Pipe.prototype[name], 'name', { value: name })
+  Object.defineProperty(create, 'name', { value: name })
+
+  /* Inject the create function in the Pipe's prototype */
+  Pipe.prototype[name] = create as any
 }
