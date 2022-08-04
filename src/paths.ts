79150d704965477604fe2b96c48168f107c3955d
Pier Fumagalli <pier@usrz.com>
2022-08-04 13:39:02 +0200

    Sync `isFile` and `isDirectory` plus resolution with `__filename`

--- src/paths.ts
+++ src/paths.ts
@@ -1,10 +1,16 @@
-import { dirname, isAbsolute, join, relative, resolve, sep } from 'node:path'
+import { statSync } from 'node:fs'
+import { createRequire } from 'node:module'
+import { dirname, extname, isAbsolute, join, relative, resolve, sep } from 'node:path'
+import { fileURLToPath, pathToFileURL } from 'node:url'
 
 import { assert } from './assert'
-import { stat } from './utils/asyncfs'
 
 /** A _branded_ `string` representing an _absolute_ path name */
 export type AbsolutePath = string & { __brand_absolute_path: never }
+
+/* ========================================================================== *
+ * PATH FUNCTIONS                                                             *
+ * ========================================================================== */
 
 /** Resolve a path into an {@link AbsolutePath} */
 export function resolveAbsolutePath(directory: AbsolutePath, ...paths: string[]): AbsolutePath {
@@ -60,32 +66,120 @@ export function getAbsoluteParent(path: AbsolutePath): AbsolutePath {
   return dirname(path) as AbsolutePath
 }
 
+/**
+ * Return the {@link process.cwd() | current working directory} as an
+ * {@link AbsolutePath}.
+ */
 export function getCurrentWorkingDirectory(): AbsolutePath {
   const cwd = process.cwd()
   assertAbsolutePath(cwd)
   return cwd
 }
 
+/* ========================================================================== *
+ * MODULE RESOLUTION FUNCTIONS                                                *
+ * ========================================================================== */
+
+/**
+ * Return the absolute path of a file relative to the given `__filename`, where
+ * `__filename` is either CommonJS's own `__filename` variable, or EcmaScript's
+ * `import.meta.url` (so either an absolute path name, or a `file:///...` url).
+ *
+ * In our build, for ESM, we _define_ the global `__filename` to be replaced by
+ * `import.meta.url` (through the magic of ESBuild's own defines), so we can
+ * safely use this for both module versions.
+ *
+ * If further `paths` are specified, those will be resolved as relative paths
+ * to the original `__filename` so we can easily write something like this:
+ *
+ * ```
+ * const dataFile = resolveFilename(__filename, 'data.json')
+ * // if we write this in "/foo/bar/baz.(ts|js|cjs|mjs)"
+ * // `dataFile` will now be "/foo/bar/data.json"
+ * ```
+ */
+export function requireFilename(__filename: string, ...paths: string[]): AbsolutePath {
+  /* Convert any "file:..." URL into a path name */
+  const file = __filename.startsWith('file:') ? fileURLToPath(__filename) : __filename
+
+  /* We should really have a proper absolute file name now */
+  assertAbsolutePath(file)
+
+  /* No paths? Return the file! */
+  if (! paths.length) return file
+
+  /* Resolve any paths, relative to the file */
+  const directory = getAbsoluteParent(file)
+  return resolveAbsolutePath(directory, ...paths)
+}
+
+/**
+ * Return the absolute path of a file which can be _required_ or _imported_
+ * by Node (or forked to via `child_process.fork`).
+ *
+ * This leverages {@link requireFilename} to figure out the starting point where
+ * to look for files, and will _try_ to match the same extension of `__filename`
+ * (so, `.ts` for `ts-node`, `.mjs` for ESM modules, ...).
+ */
+export function requireResolve(__filename: string, module: string): AbsolutePath {
+  const file = requireFilename(__filename)
+
+  // We do our custom resolution _only_ for local (./foo.bar) files...
+  if (module.match(/^\.\.?\//)) {
+    // If we import "../foo.ext" from "/a/b/c/bar.ts" we need to check:
+    // * /a/b/foo.ext
+    // * /a/b/foo.ext.ts
+    // * /a/b/foo.ext/index.ts
+    // ... then delegate to the standard "require.resolve(...)"
+    const url = pathToFileURL(file)
+    const ext = extname(file)
+    const checks = ext ? [ `${module}`, `${module}${ext}`, `${module}/index${ext}` ] : [ module ]
+
+    for (const check of checks) {
+      const resolved = fileURLToPath(new URL(check, url)) as AbsolutePath
+      if (isFile(resolved)) {
+        module = check
+        break
+      }
+    }
+  }
+
+  const require = createRequire(file)
+  const required = require.resolve(module)
+  assertAbsolutePath(required)
+  return required
+}
+
+/* ========================================================================== *
+ * FILE CHECKING FUNCTIONS                                                    *
+ * ========================================================================== */
+
 /**
  * Resolves the specified path as an {@link AbsolutePath} and checks it is a
  * _file_, returning `undefined` if it is not.
  */
-export function isFile(path: AbsolutePath, ...paths: string[]): Promise<AbsolutePath | undefined> {
+export function isFile(path: AbsolutePath, ...paths: string[]): AbsolutePath | undefined {
   const file = resolveAbsolutePath(path, ...paths)
-  return stat(file).then((s) => s.isFile() ? file : undefined, (error) => {
-    if (error.code === 'ENOENT') return undefined
-    throw error
-  })
+  try {
+    const stat = statSync(file)
+    if (stat.isFile()) return file
+  } catch (error: any) {
+    if (error.code !== 'ENOENT') throw error
+  }
+  return undefined
 }
 
 /**
  * Resolves the specified path as an {@link AbsolutePath} and checks it is a
  * _directory_, returning `undefined` if it is not.
  */
-export function isDirectory(path: AbsolutePath, ...paths: string[]): Promise<AbsolutePath | undefined> {
-  const dir = resolveAbsolutePath(path, ...paths)
-  return stat(dir).then((s) => s.isDirectory() ? dir : undefined, (error) => {
-    if (error.code === 'ENOENT') return undefined
-    throw error
-  })
+export function isDirectory(path: AbsolutePath, ...paths: string[]): AbsolutePath | undefined {
+  const directory = resolveAbsolutePath(path, ...paths)
+  try {
+    const stat = statSync(directory)
+    if (stat.isDirectory()) return directory
+  } catch (error: any) {
+    if (error.code !== 'ENOENT') throw error
+  }
+  return undefined
 }
