bc5e7be6726d465fc5306fa1405f4d6823bed9a8
Pier Fumagalli <pier@usrz.com>
2022-08-05 17:11:25 +0200

    Use our reports

--- src/plugs/tsc.ts
+++ src/plugs/tsc.ts
@@ -1,11 +1,9 @@
-import type { CompilerOptions } from 'typescript'
-
 import ts from 'typescript' // TypeScript does NOT support ESM modules
 
 import { fail } from 'assert'
 import { Files } from '../files'
-import { $p, log } from '../log'
-import { isFile } from '../paths'
+import { $p, log, Report, ReportRecord } from '../log'
+import { AbsolutePath, getCurrentWorkingDirectory, isFile, resolveAbsolutePath } from '../paths'
 import { install, Plug } from '../pipe'
 import { Run } from '../run'
 import { ParseOptions, parseOptions } from '../utils/options'
@@ -15,14 +13,14 @@ import { getCompilerOptions } from './tsc/options'
 
 export class Tsc implements Plug<Files> {
   private readonly _tsconfig?: string
-  private readonly _options: CompilerOptions
+  private readonly _options: ts.CompilerOptions
 
   constructor()
   constructor(config: string)
-  constructor(options: CompilerOptions)
-  constructor(config: string, options: CompilerOptions)
+  constructor(options: ts.CompilerOptions)
+  constructor(config: string, options: ts.CompilerOptions)
 
-  constructor(...args: ParseOptions<CompilerOptions>) {
+  constructor(...args: ParseOptions<ts.CompilerOptions>) {
     const { params: [ tsconfig ], options } = parseOptions(args, {})
     this._tsconfig = tsconfig
     this._options = options
@@ -33,7 +31,7 @@ export class Tsc implements Plug<Files> {
       run.resolve(this._tsconfig) :
       isFile(files.directory, 'tsconfig.json')
 
-    const overrides: CompilerOptions = {
+    const overrides: ts.CompilerOptions = {
       rootDir: files.directory, // by default, our "files" directory
       ...this._options, // any other options specified in the constructor
     }
@@ -43,14 +41,20 @@ export class Tsc implements Plug<Files> {
         overrides, // overrides from constructor, might be an empty object
         run.buildFile) // overrides are defined in the build file, sooo.....
 
+    const report = run.log.report('TypeScript Report')
+
+    // Update report and fail on errors
+    updateReport(report, errors, getCurrentWorkingDirectory())
+    if (report.errors) report.emit(true).fail()
+
     const { rootDir, outDir } = options
     const root = rootDir ? run.resolve(rootDir) : files.directory
     const out = outDir ? run.resolve(outDir) : root
 
-    const host = new TypeScriptHost(root, run.log)
+    const host = new TypeScriptHost(root)
 
     const paths = [ ...files.absolutePaths() ]
-    for (const path of paths) log.trace(`Compiling "${path}"`)
+    for (const path of paths) log.trace(`Compiling "${$p(path)}"`)
 
     // Get our build file and create the master program
     log.info('Compiling', paths.length, 'files')
@@ -58,18 +62,23 @@ export class Tsc implements Plug<Files> {
 
     const program = ts.createProgram(paths, options, host, undefined, errors)
     const diagnostics = ts.getPreEmitDiagnostics(program)
-    host.checkDiagnostics(diagnostics)
+
+    // Update report and fail on errors
+    updateReport(report, diagnostics, root)
+    if (report.errors) report.emit(true).fail()
 
     const builder = run.files(out)
     const promises: Promise<void>[] = []
     const result = program.emit(undefined, (fileName, code) => {
-      promises.push(builder.write(fileName, code).then((file) => {
-        log.trace('Written', $p(file))
-      }))
+      console.log('FILENAME', fileName)
+      void code
+      // promises.push(builder.write(fileName, code).then((file) => {
+      //   log.trace('Written', $p(file))
+      // }))
     })
 
-    // Check for errors...
-    host.checkDiagnostics(result.diagnostics)
+    updateReport(report, result.diagnostics, root)
+    if (report.errors) report.emit(true).fail()
 
     const settlements = await Promise.allSettled(promises)
     for (const settlement of settlements) {
@@ -92,4 +101,77 @@ declare module '../pipe' {
   export interface Pipe {
     tsc: PipeExtension<typeof Tsc>
   }
+}
+
+/* ========================================================================== *
+ * INTERNAL                                                                   *
+ * ========================================================================== */
+
+function convertMessageChain(chain: ts.DiagnosticMessageChain, indent = 0): string[] {
+  const message = `${''.padStart(indent * 2)}chain.messageText`
+
+  if (chain.next) {
+    const next = chain.next.map((c) => convertMessageChain(c, indent + 1))
+    return [ message, ...next.flat(1) ]
+  } else {
+    return [ message ]
+  }
+}
+
+function convertDiagnostics(
+    diagnostics: readonly ts.Diagnostic[],
+    directory: AbsolutePath,
+): ReportRecord[] {
+  return diagnostics.map((diagnostic): ReportRecord => {
+    // console.log(diagnostic)
+    void directory
+
+    // Convert the `DiagnosticCategory` to our level
+    let level: 'NOTICE' | 'ERROR' | 'WARN'
+    switch (diagnostic.category) {
+      case ts.DiagnosticCategory.Error: level = 'ERROR'; break
+      case ts.DiagnosticCategory.Warning: level = 'WARN'; break
+      default: level = 'NOTICE'
+    }
+
+    // Convert the `messageText` to a string
+    let message: string | string[]
+    if (typeof diagnostic.messageText === 'string') {
+      message = diagnostic.messageText
+    } else {
+      message = convertMessageChain(diagnostic.messageText)
+    }
+
+    // Simple variables
+    const tags = `TS${diagnostic.code}`
+
+
+    if (diagnostic.file) {
+      const { file: sourceFile, start, length } = diagnostic
+      const file = resolveAbsolutePath(directory, sourceFile.fileName)
+      const source = sourceFile.getFullText()
+
+      if (start !== undefined) {
+        const position = sourceFile.getLineAndCharacterOfPosition(start)
+        let { line, character: column } = position
+        column += 1
+        line += 1
+
+        return { level, message, tags, file, source, line, column, length }
+      } else {
+        return { level, message, tags, file, source }
+      }
+    } else {
+      return { level, message, tags }
+    }
+  })
+}
+
+function updateReport(
+    report: Report,
+    diagnostics: readonly ts.Diagnostic[],
+    directory: AbsolutePath,
+): void {
+  const records = convertDiagnostics(diagnostics, directory)
+  report.add(...records)
 }
