7223a96712d17a805c5b774ce25ccd7e84ef37d1
Pier Fumagalli <pier@usrz.com>
2022-08-08 20:40:27 +0200

    Use esbuild warning mechanism

--- src/plugs/esbuild/bundle-locals.ts
+++ src/plugs/esbuild/bundle-locals.ts
@@ -1,23 +1,27 @@
 import type { Plugin } from 'esbuild'
 
-import { $und, log } from '../../log'
-
 /**
  * A simple ESBuild plugin bundling _only_ local files, and marking anything
  * imported from `node_modules` as _external_.
  */
 export function bundleLocals(): Plugin {
+  let disabled = false
+
   return {
     name: 'bundle-locals',
 
     setup(build): void {
-      if (! build.initialOptions.bundle) {
-        log.warn(`ESBuild ${$und('bundle-locals')} plugin disabled when not bundling`)
-        return
-      }
+      build.onStart(() => {
+        if (build.initialOptions.bundle) return
+
+        disabled = true
+        return { warnings: [ { text: 'Plugin disabled when not bundling' } ] }
+      })
 
       /* Intercept resolution */
       build.onResolve({ filter: /.*/ }, (args) => {
+        if (disabled) return
+
         /* Ignore the entry points (when the file is not being imported) */
         if (! args.importer) return null
 
