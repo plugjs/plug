49fbd8d7a0ac87e744d76403b3b2f9cba32197cc
Pier Fumagalli <pier@usrz.com>
2022-08-09 22:05:45 +0200

    Moving symbols into a proper place

--- src/log/constants.ts
+++
@@ -1,2 +0,0 @@
-/** Constant thrown indicating a build failure already logged */
-export const buildFailed = Symbol.for('plugjs:build.failed')
---
+++ src/symbols.ts
@@ -0,0 +1,5 @@
+/** Constant thrown indicating a build failure already logged */
+export const buildFailed = Symbol.for('plugjs:buildFailed')
+
+/** Symbol indicating that an object is a Build */
+export const buildMarker = Symbol.for('plugjs:isBuild')
