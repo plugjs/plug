65b3733b4fec89bb0274e86ddd56cf5319a0f760
Pier Fumagalli <pier@usrz.com>
2022-08-10 19:03:57 +0200

    Slightly better loader

--- extra/ts-loader.cjs
+++ extra/ts-loader.cjs
@@ -6,8 +6,8 @@ const { buildSync, formatMessagesSync } = require('esbuild')
  * HACK BEYOND REDEMPTION: TRANSPILE .ts FILES                                *
  * ========================================================================== */
 
+const _path = require('node:path')
 const _module = require('node:module')
-
 const _debug = process.env.DEBUG_TS_LOADER === 'true'
 
 function _report(filename, what) {
@@ -37,30 +37,43 @@ if (! ('.ts' in _module._extensions)) {
 
   _module._extensions['.ts'] = (_mod, filename) => {
     if (_debug) console.log(`Compiling "${filename}"`)
+    const file = _path.basename(filename)
+    const dir = _path.dirname(filename)
 
     let result
     try {
       result = buildSync({
-        entryPoints: [ filename ],
+        absWorkingDir: dir,
+        outdir: dir,
+        entryPoints: [ file ],
         sourcemap: 'inline',
+        sourcesContent: false,
         platform: 'node',
+        outExtension: { '.js': '.ts' },
         target: `node${process.versions['node']}`,
         format: 'cjs',
+        allowOverwrite: true,
         write: false,
       })
     } catch (cause) {
       _report(filename, cause)
-      throw new Error(`ESBuild errors in "${filename}"`, { cause })
+      throw new Error(`ESBuild errors compiling "${filename}"`, { cause })
     }
 
     _report(filename, result)
 
-    if (result.outputFiles.length !== 1) {
-      throw new Error(`ESBuild produced ${result.outputFiles.length} files for "${filename}"`)
+    const output = result.outputFiles.reduce((output, current) => {
+      output[current.path] = current.text
+      return output
+    }, {})
+
+    if (!(filename in output)) {
+      console.log(Object.keys(output))
+      throw new Error(`ESBuild produced no output for "${filename}"`)
     }
 
     try {
-      _mod._compile(result.outputFiles[0].text, filename)
+      _mod._compile(output[filename], filename)
     } catch (cause) {
       throw new Error(`Error compiling module "${filename}"`, { cause })
     }
