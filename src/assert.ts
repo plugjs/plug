efeec4fb13a649692e1574f53af8fb4120d3e17d
Pier Fumagalli <pier@usrz.com>
2022-08-10 22:36:59 +0200

    Throw proper errors

--- src/assert.ts
+++ src/assert.ts
@@ -1,22 +1,46 @@
+import { inspect } from 'node:util'
+
 /* ========================================================================== *
  * BUILD FAILURES                                                             *
  * ========================================================================== */
 
+const buildFailure = Symbol.for('plugjs:isBuild')
+
+/** Check if the specified build is actually a {@link Build} */
+export function isBuildFailure(arg: any): arg is BuildFailure {
+  return arg && arg[buildFailure] === buildFailure
+}
+
+/** An error produced in our build, with proper inspection for logging */
+export class BuildError extends Error {
+  constructor(message: string) {
+    super(message)
+  }
+
+  [inspect.custom](): string {
+    return this.message
+  }
+}
+
+/** A {@link BuildFailure} represents an error _already logged_ in our build. */
+export class BuildFailure extends BuildError {
+  constructor() {
+    super('Build Failure')
+    Object.defineProperty(this, buildFailure, { value: buildFailure })
+  }
+}
+
 /** Asserts something as _truthy_ and fail the build if not */
 export function assert(assertion: any, message: string): asserts assertion {
-  if (! assertion) throw failure(message)
+  if (! assertion) throw new BuildError(message)
 }
 
-export function fail(message: string): never
-export function fail(message: string): never
+/** Throw a {@link BuildError} (an {@link Error} printed nicely) */
 export function fail(message: string): never {
-  throw failure(message)
+  throw new BuildError(message)
 }
 
-export function failure(): Error
-export function failure(message: string): Error
-export function failure(message: string, cause: any): Error
-export function failure(message?: string, cause?: any): Error {
-  void message, cause
-  return new Error()
+/** Return a non-logged {@link BuildFailure} */
+export function failure(): BuildFailure {
+  return new BuildFailure()
 }
