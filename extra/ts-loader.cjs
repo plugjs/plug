1c0237f555f45f6a75d7a0a37ab209175478f295
Pier Fumagalli <pier@usrz.com>
2022-08-11 14:47:36 +0200

    Cleanup our CJS TypeScript loader (in prep for ESM)

--- extra/ts-loader.cjs
+++ extra/ts-loader.cjs
@@ -1,81 +1,113 @@
 'use strict'
 
-const { buildSync, formatMessagesSync } = require('esbuild')
-
 /* ========================================================================== *
- * HACK BEYOND REDEMPTION: TRANSPILE .ts FILES                                *
+ * HACK BEYOND REDEMPTION: TRANSPILE .ts FILES (the cjs version)              *
+ * -------------------------------------------------------------------------- *
+ * This is fairly simple, and relies on Node's own `require.extensions`       *
+ * mechanism. We don't need to do anything fancy here, when the loader is     *
+ * registered in the extensions, Node will automagically figure out what file *
+ * to import when doing things like `require('foo')`                          *
  * ========================================================================== */
 
-const _path = require('node:path')
-const _module = require('node:module')
 const _debug = process.env.DEBUG_TS_LOADER === 'true'
 
+const _esbuild = require('esbuild')
+const _path = require('node:path')
+
+/**
+ * Take an ESBuild `BuildResult` or `BuildFailure` (they both have arrays
+ * of `Message` in both `warnings` and `errors`), format them and print them
+ * out nicely. Then fail if any error was detected.
+ */
 function _report(filename, what) {
-  const terminalWidth = process.stdout.columns || 80
-  const color = !! process.stdout.isTTY
+  const { warnings = [], errors = [] } = what
 
-  const messages = []
+  const output = process.stderr
+  const options = { color: !!output.isTTY, terminalWidth: output.columns || 80 }
 
-  if (what.warnings && what.warnings.length) {
-    const m = formatMessagesSync(what.warnings, { kind: 'warning', color, terminalWidth })
-    messages.push(...m)
-  }
+  const messages = [
+    ... _esbuild.formatMessagesSync(warnings, { kind: 'warning', ...options }),
+    ... _esbuild.formatMessagesSync(errors, { kind: 'error', ...options }),
+  ]
 
-  if (what.errors && what.errors.length) {
-    const m = formatMessagesSync(what.errors, { kind: 'error', color, terminalWidth })
-    messages.push(...m)
-  }
+  messages.forEach((message) => output.write(`${message}\n`))
 
-  for (const message in messages) process.stdout.write(message)
-  if (what.errors && what.errors.length) {
-    throw new Error(`ESBuild errors in "${filename}"`, { cause })
+  if (errors.length) {
+    const message = `[ts-loader] ESBuild found ${errors.length} errors in "${filename}"`
+    throw new Error(message, { cause: what })
   }
 }
 
-if (! ('.ts' in _module._extensions)) {
-  if (_debug) console.log(`Installing TS loader from "${__filename}"`)
+/** Register our ".ts" extension loader for CJS modules. */
+if (! ('.ts' in require.extensions)) {
+  if (_debug) console.log(`[ts-loader cjs]: Installing TS loader from "${__filename}"`)
 
-  _module._extensions['.ts'] = (_mod, filename) => {
-    if (_debug) console.log(`Compiling "${filename}"`)
+  require.extensions['.ts'] = (module, filename) => {
+    if (_debug) console.log(`[ts-loader cjs]: Transpiling "${filename}"`)
+
+    /* We always operate relative to the file */
     const file = _path.basename(filename)
     const dir = _path.dirname(filename)
 
-    let result
-    try {
-      result = buildSync({
-        absWorkingDir: dir,
-        outdir: dir,
-        entryPoints: [ file ],
-        sourcemap: 'inline',
-        sourcesContent: false,
-        platform: 'node',
-        outExtension: { '.js': '.ts' },
-        target: `node${process.versions['node']}`,
-        format: 'cjs',
-        allowOverwrite: true,
-        write: false,
-      })
-    } catch (cause) {
-      _report(filename, cause)
-      throw new Error(`ESBuild errors compiling "${filename}"`, { cause })
+    /* ESbuild options */
+    const options = {
+      format: 'cjs', // here we are transpiling as CJS
+      entryPoints: [ file ], // relative file name
+      absWorkingDir: dir, // directory where file lives
+      outdir: dir, // output in the same directory
+      sourcemap: 'inline', // always inline source maps
+      sourcesContent: false, // do not include sources content in sourcemap
+      platform: 'node', // d'oh! :-)
+      target: `node${process.versions['node']}`, // target _this_ version
+      outExtension: { '.js': '.ts' }, // keep the output file name
+      allowOverwrite: true, // input and output file names are the same
+      write: false, // we definitely _do not_ write this back to disk
+      define: { // those are defined/documented in "./globals.ts"
+        __fileurl: '__filename',
+        __esm: 'false',
+        __cjs: 'true',
+      },
     }
 
+    /* Emit a line on the console when loading in debug mode */
+    if (_debug) options.banner = {
+      js: 'console.log(`[ts-loader cjs]: Loaded "${__filename}" (pid=${process.pid})`);'
+    }
+
+    /* Transpile our TypeScript file into some JavaScript stuff */
+    let result
+    try {
+      result = _esbuild.buildSync(options)
+    } catch (cause) {
+      _report(filename, cause)
+      // If the above doesn't fail (normal error?) then bail out
+      throw new Error(`[ts-loader] ESBuild error transpiling "${filename}"`, { cause })
+    }
+
+    /* Report out any warning or error abd fail if there are errors */
     _report(filename, result)
 
+    /* Normalize the output of the result into { filename: code } */
     const output = result.outputFiles.reduce((output, current) => {
       output[current.path] = current.text
       return output
     }, {})
 
+    /* Make sure we _do_ have some code transpiled */
     if (!(filename in output)) {
-      console.log(Object.keys(output))
-      throw new Error(`ESBuild produced no output for "${filename}"`)
+      throw new Error(`[ts-loader] ESBuild produced no output for "${filename}"`)
     }
 
+    /* Let node do its thing, but wrap any error it throws */
+    const code = output[filename]
     try {
-      _mod._compile(output[filename], filename)
+      module._compile(code, filename)
     } catch (cause) {
-      throw new Error(`Error compiling module "${filename}"`, { cause })
+      if (_debug) {
+        console.log('[ts-loader cjs] >', filename)
+        console.log(code.replace(/^/gm, '[ts-loader cjs] | '))
+      }
+      throw new Error(`[ts-loader] Error compiling module "${filename}"`, { cause })
     }
   }
 }
