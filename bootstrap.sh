ae2a9357e1fe995c25c7ac51e9e79560a6cc3c1a
Pier Fumagalli <pier@usrz.com>
2022-08-12 16:15:03 +0200

    Compile loader on bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,25 @@
 #!/bin/sh
 
+# Relative to our build
+pushd "$(dirname $0)"
+
+# Compile our "ts-loader" loader
+./node_modules/.bin/esbuild \
+	--platform=node \
+	--format=esm \
+	--target=node16 \
+	--outfile=./extra/ts-loader.mjs \
+	--sourcemap=linked \
+	--sources-content=false \
+		./extra/ts-loader.mts || {
+			echo "Error compiling loader"
+			exit 1
+		}
+
+
+# Bootstrap with our loader
 node \
-	--require=./extra/ts-loader.cjs \
 	--experimental-loader=./extra/ts-loader.mjs \
 	--no-warnings \
-		./src/cli.ts
+		./src/cli.ts "${@}"
+exit $?
