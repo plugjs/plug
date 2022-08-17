08963cded895db0b6c7cf4c7d008b5d979fe75de
Pier Fumagalli <pier@usrz.com>
2022-08-17 14:40:05 +0200

    Better defaults: module type and node version

--- src/plugs/esbuild.ts
+++ src/plugs/esbuild.ts
@@ -1,10 +1,12 @@
-import { build, BuildFailure, BuildOptions, BuildResult, Message, Metafile } from 'esbuild'
+import { build, BuildFailure, BuildOptions, BuildResult, Format, Message, Metafile } from 'esbuild'
+import { basename } from 'node:path'
 import { assert } from '../assert.js'
 import { Files, FilesBuilder } from '../files.js'
-import { $p, ERROR, ReportLevel, ReportRecord, WARN } from '../log.js'
-import { AbsolutePath, resolveAbsolutePath } from '../paths.js'
+import { $p, ERROR, Logger, ReportLevel, ReportRecord, WARN } from '../log.js'
+import { AbsolutePath, getAbsoluteParent, resolveAbsolutePath } from '../paths.js'
 import { install, Plug } from '../pipe.js'
 import { Run } from '../run.js'
+import { readFile } from '../utils/asyncfs.js'
 
 export type ESBuildOptions = Omit<BuildOptions, 'absWorkingDir' | 'entryPoints' | 'watch'>
 
@@ -20,20 +22,23 @@ export class ESBuild implements Plug<Files> {
     const absWorkingDir = files.directory
 
     const options: BuildOptions = {
-      /* Defaults */
+      /* Default our platform/target to NodeJS, current major version */
       platform: 'node',
-      target: `node${process.versions['node']}`,
-      format: 'cjs',
+      target: `node${process.versions['node'].split('.')[0]}`,
+
+      /* The default format (if not specified in options) is from package.json */
+      format: this._options.format || await _moduleFormat(files.directory, run.log),
+
+      /* Output bese directory */
       outbase: absWorkingDir,
 
-      logLevel: 'silent',
-
-      /* Our options */
+      /* Merge in the caller's options */
       ...this._options,
 
       /* Always override */
       absWorkingDir,
       entryPoints,
+      logLevel: 'silent',
       watch: false,
     }
 
@@ -126,3 +131,57 @@ declare module '../pipe.js' {
 export * from './esbuild/bundle-locals.js'
 export * from './esbuild/check-dependencies.js'
 export * from './esbuild/fix-extensions.js'
+
+/* ========================================================================== *
+ * DEFAULT MODULE FORMAT FROM PACKAGE.JSON                                    *
+ * ========================================================================== */
+
+/** Cache for directory to module format as discovered in "package.json" */
+const _moduleFormatCache = new Map<AbsolutePath, Format>()
+
+/**
+ * Figures out the _default_ module type for a directory, looking into the
+ * `package.json`'s `type` field (either `commonjs` or `module`)
+ */
+async function _moduleFormat(directory: AbsolutePath, log: Logger): Promise<Format> {
+  /* Before doing anything else, check our cache */
+  const type = _moduleFormatCache.get(directory)
+  if (type) return type
+
+  /* Try to read the "package.json" file from this directory */
+  const file = resolveAbsolutePath(directory, 'package.json')
+
+  try {
+    const json = await readFile(file, 'utf-8')
+    const data = JSON.parse(json)
+
+    /* Be liberal in what you accept? Default to CommonJS if none found */
+    const type = data.type === 'module' ? 'esm' : 'cjs'
+    log.debug(`File "${file}" defines module type as "${data.type}" (${type})`)
+    _moduleFormatCache.set(directory, type)
+    return type
+  } catch (cause: any) {
+    /* We _accept_ a couple of errors, file not found, or file is directory */
+    if ((cause.code !== 'ENOENT') && (cause.code !== 'EISDIR')) throw cause
+  }
+
+  /*
+   * We couldn't find "package.json" in this directory, go up if we can!
+   *
+   * That said, if we are at a directory called "node_modules" we stop here,
+   * as we don't want to inherit the default type from an _importing_ package,
+   * into the _imported_ one...
+   */
+  const name = basename(directory)
+  const parent = getAbsoluteParent(directory)
+
+  if ((name === 'node_modules') || (parent === directory)) {
+    _moduleFormatCache.set(directory, 'cjs') // default
+    return 'cjs'
+  } else {
+    /* We also cache back, on the way up */
+    const type = await _moduleFormat(parent, log)
+    _moduleFormatCache.set(directory, type)
+    return type
+  }
+}
