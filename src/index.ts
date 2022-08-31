a0d3861f7f51ce62745119d5d1d8bfc76e4fa5a9
Pier Fumagalli <pier@usrz.com>
2022-08-31 13:22:21 +0200

    Reorganize imports & exports

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,15 @@
 /// <reference path="../extra/webassembly.d.ts" />
 
-// Our minimal exports
-export * from './assert.js'
-export * from './build.js'
-export * from './plugs.js'
-export * from './log.js'
-export * from './helpers.js'
+export type { AbsolutePath } from './paths'
+export type { Files, FilesBuilder } from './files'
+export type { Pipe } from './pipe'
 
-// Utility types
-export type { MatchOptions, MatchResult } from './utils/match.js'
-export type { ParseOptions } from './utils/options.js'
-export type { WalkOptions } from './utils/walk.js'
+// Our minimal exports
+export * from './assert'
+export * from './build'
+export * from './helpers'
+export * from './log'
 
 // PlugJS types
-export type { AbsolutePath } from './paths.js'
-export type { Files, FilesBuilder } from './files.js'
-export type { FindOptions, Run } from './run.js'
-export type { Pipe, Plug, PlugFunction } from './pipe.js'
-export type { Task } from './task.js'
+export * from './plugs'
+export * from './types'
