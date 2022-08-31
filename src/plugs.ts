3e52a46c121074d70cca6510e4f31b8cff9a4804
Pier Fumagalli <pier@usrz.com>
2022-08-31 18:00:20 +0200

    New install for forking `Mocha`.

--- src/plugs.ts
+++ src/plugs.ts
@@ -5,17 +5,14 @@
 import { requireResolve } from './paths'
 import { installForking } from './pipe'
 
-import type Mocha from './plugs/mocha/runner'
 import type Tsc from './plugs/tsc/runner'
 
 declare module './pipe' {
   export interface Pipe {
-    mocha: PipeExtension<typeof Mocha>
     tsc: PipeExtension<typeof Tsc>
   }
 }
 
-installForking('mocha', requireResolve(__fileurl, './plugs/mocha/runner'))
 installForking('tsc', requireResolve(__fileurl, './plugs/tsc/runner'))
 
 /* ========================================================================== *
@@ -23,6 +20,7 @@ installForking('tsc', requireResolve(__fileurl, './plugs/tsc/runner'))
  * ========================================================================== */
 
 export * from './plugs/eslint'
+export * from './plugs/mocha'
 
 export * from './plugs/copy'
 export * from './plugs/coverage'
--- src/plugs/mocha/runner.ts
+++ src/plugs/mocha/runner.ts
@@ -3,37 +3,14 @@ import RealMocha from 'mocha' // Mocha types pollute the global scope!
 import { failure } from '../../assert'
 import { Files } from '../../files'
 import { $wht, NOTICE } from '../../log'
+import { PipeParameters } from '../../pipe'
 import { Plug, RunContext } from '../../types'
+import { MochaOptions } from '../mocha'
 import { logSymbol, PlugReporter, runSymbol } from './reporter'
-
-/** Options to construct our {@link Mocha} plug. */
-export interface MochaOptions {
-  /** Specify the directory where coverage data will be saved */
-  coverageDir?: string,
-  /** Bail after first test failure? */
-  bail?: boolean,
-  /** Show diff on failure? */
-  diff?: boolean,
-  /** Report tests without running them? */
-  dryRun?: boolean,
-  /** Tests marked `only` fail the suite? */
-  forbidOnly?: boolean,
-  /** Pending tests fail the suite? */
-  forbidPending?: false,
-  /** Reporter name. */
-  reporter?: string
-  /** Options for the reporter */
-  reporterOptions?: Record<string, any>,
-  /** Number of times to retry failed tests. */
-  retries?: number,
-  /** Slow threshold value. */
-  slow?: number,
-  /** Timeout threshold value. */
-  timeout?: number,
-}
 
 /** Writes some info about the current {@link Files} being passed around. */
 export default class Mocha implements Plug<undefined> {
+  constructor(...args: PipeParameters<'mocha'>)
   constructor(private readonly _options: MochaOptions = {}) {}
 
   async pipe(files: Files, run: RunContext): Promise<undefined> {
