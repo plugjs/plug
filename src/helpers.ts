6f41a0b1f27cf779b6f331d2a2f7e97436230338
Pier Fumagalli <pier@usrz.com>
2022-09-02 17:52:02 +0200

    Simply use `Pipe` from `pipe.ts` (no longer abstract).

--- src/helpers.ts
+++ src/helpers.ts
@@ -3,40 +3,10 @@ import { requireContext } from './async'
 import { Files } from './files'
 import { $p, log } from './log'
 import { AbsolutePath, commonPath, getCurrentWorkingDirectory, resolveDirectory, resolveFile } from './paths'
-import { Context, Pipe, Plug, PlugFunction, PlugResult } from './pipe'
+import { Pipe } from './pipe'
 import { rm } from './utils/asyncfs'
 import { ParseOptions, parseOptions } from './utils/options'
 import { walk, WalkOptions } from './utils/walk'
-
-/* ========================================================================== *
- * INTERNAL SIMPLE PIPE IMPLEMENTATION                                        *
- * ========================================================================== */
-
-class PipeImpl extends Pipe implements Pipe {
-  constructor(private readonly _start: (context: Context) => Promise<Files>) {
-    super()
-  }
-
-  plug(plug: Plug<Files>): Pipe
-  plug(plug: PlugFunction<Files>): Pipe
-  plug(plug: Plug<void | undefined>): Promise<undefined>
-  plug(plug: PlugFunction<void | undefined>): Promise<undefined>
-  plug(arg: Plug<PlugResult> | PlugFunction<PlugResult>): Pipe | Promise<undefined> {
-    const plug = typeof arg === 'function' ? { pipe: arg } : arg
-
-    const parent = this
-    return new PipeImpl(async (context: Context): Promise<Files> => {
-      const files = await parent._start(context)
-      const files2 = await plug.pipe(files, context)
-      assert(files2, 'Unable to extend pipe (part tres)')
-      return files2
-    })
-  }
-
-  async run(): Promise<Files> {
-    return this._start(requireContext())
-  }
-}
 
 /* ========================================================================== *
  * EXTERNAL HELPERS                                                           *
@@ -60,7 +30,8 @@ export function find(glob: string, ...extra: [...globs: string[], options: FindO
 export function find(...args: ParseOptions<FindOptions>): Pipe {
   const { params: globs, options } = parseOptions(args, {})
 
-  return new PipeImpl(async (context: Context): Promise<Files> => {
+  const context = requireContext()
+  return new Pipe(context, async (): Promise<Files> => {
     const directory = options.directory ?
       context.resolve(options.directory) :
       getCurrentWorkingDirectory()
@@ -90,7 +61,8 @@ export function find(...args: ParseOptions<FindOptions>): Pipe {
  * ```
  */
 export function merge(pipes: (Pipe | Files | Promise<Files>)[]): Pipe {
-  return new PipeImpl(async (): Promise<Files> => {
+  const context = requireContext()
+  return new Pipe(context, async (): Promise<Files> => {
     if (pipes.length === 0) return Files.builder(getCurrentWorkingDirectory()).build()
 
     const [ first, ...other ] = await Promise.all(pipes.map((pipe) => {
