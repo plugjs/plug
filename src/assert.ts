d6c15d8ba8427f84272b8250297913f97ba569e7
Pier Fumagalli <pier@usrz.com>
2022-10-25 13:41:38 +0200

    Await for settlement and assert resolution/rejection

--- src/assert.ts
+++ src/assert.ts
@@ -2,6 +2,9 @@
  * BUILD FAILURES                                                             *
  * ========================================================================== */
 
+import { currentContext } from './async'
+import { log } from './log'
+
 const buildError = Symbol.for('plugjs:buildError')
 const buildFailure = Symbol.for('plugjs:buildFailure')
 
@@ -29,6 +32,41 @@ export class BuildFailure extends Error {
     super('Build Failure')
     Object.defineProperty(this, buildFailure, { value: buildFailure })
   }
+}
+
+/** Await and assert that all specified promises were fulfilled */
+export async function assertPromises<T>(promises: (T | Promise<T>)[], message: string): Promise<T[]> {
+  // Await for the settlement of all the promises
+  const settlements = await Promise.allSettled(promises)
+
+  // Separate the good from the bad... Here we don't report BuildFailures,
+  // but we keep track of them, henceforth we keep track of both "hasFailed"
+  // as a flag and "failures" as a set of what needs to be logged
+  const results: T[] = []
+  const failures = new Set<any>()
+  let hasFailed = false
+
+  settlements.forEach((settlement) => {
+    if (settlement.status === 'fulfilled') {
+      results.push(settlement.value)
+    } else {
+      hasFailed = true // mark failures
+      const failure = settlement.reason
+      if (! isBuildFailure(failure)) failures.add(failure)
+    }
+  })
+
+  // Check for errors and report/fail if anything happened
+  if (hasFailed) {
+    const context = currentContext()
+    const logger = context ? context.log : log
+    failures.forEach((failure) => {
+      logger.error(message, failure)
+    })
+    throw failure()
+  }
+
+  return results
 }
 
 /** Asserts something as _truthy_ and fail the build if not */
