de26dd0641d8c3aa8c8015754f71e834597d9567
Pier Fumagalli <pier@usrz.com>
2022-10-26 11:44:37 +0200

    Install `eslint-plugin-import` and update dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,4 +12,21 @@ module.exports = {
       // './test-d/tsconfig.json',
     ],
   },
+  plugins: [ 'import' ],
+  settings: {
+    'import/extensions': [ '.ts' ],
+    'import/external-module-folders': [ 'node_modules', 'node_modules/@types' ],
+    'import/parsers': {
+      '@typescript-eslint/parser': [ '.ts' ],
+    },
+    'import/resolver': {
+      'node': {
+        'extensions': [ '.ts' ],
+      },
+    },
+  },
+
+  rules: {
+    'import/no-cycle': 'error',
+  },
 }
