cb736e803125c42b15c366626fad0edc8e654036
Pier Fumagalli <pier@usrz.com>
2023-02-20 16:08:03 +0100

    Skeleton for monorepo

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+'use strict'
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:@plugjs/typescript',
+  ],
+  parserOptions: {
+    project: [
+      './tsconfig.json',
+      './workspaces/cov8/tsconfig.json',
+      './workspaces/cov8/test/tsconfig.json',
+      './workspaces/eslint/tsconfig.json',
+      './workspaces/eslint/test/tsconfig.json',
+      './workspaces/jasmine/tsconfig.json',
+      './workspaces/jasmine/test/tsconfig.json',
+      './workspaces/plug/tsconfig.json',
+      './workspaces/plug/test/tsconfig.json',
+      './workspaces/typescript/tsconfig.json',
+      './workspaces/typescript/test/tsconfig.json',
+    ],
+  },
+}
