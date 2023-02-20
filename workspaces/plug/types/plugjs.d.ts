e6f8fa817431d26a7ed71b8cf5825ae260df4acd
Pier Fumagalli <pier@usrz.com>
2023-02-20 23:58:00 +0100

    What in the?!?!!?

--- workspaces/plug/types/plugjs.d.ts
+++ workspaces/plug/types/plugjs.d.ts
@@ -1,5 +1,3 @@
-/// <reference no-default-lib="true"/>
-
 /**
  * A pseudo-variable replaced by `ESBuild` resolving to either `__filename`
  * in CJS modules, or to `import.meta.url` in ESM modules.
