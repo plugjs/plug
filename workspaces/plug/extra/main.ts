eb49650bf0c1b17e05d8f467bcf532354f890b55
Pier Fumagalli <pier@usrz.com>
2023-02-22 16:53:48 +0100

    Extract `main` functionality from CLI

---
+++ workspaces/plug/extra/main.ts
@@ -0,0 +1,112 @@
+/* eslint-disable no-console */
+import _childProcess from 'node:child_process'
+import _path from 'node:path'
+import _url from 'node:url'
+import _util from 'node:util'
+
+// Debugging
+const debug = _util.debuglog('plug:main')
+
+/* ========================================================================== *
+ * TS LOADER FORCE TYPE                                                       *
+ * ========================================================================== */
+
+const tsLoaderMarker = Symbol.for('plugjs:tsLoader')
+
+function forceType(type: 'commonjs' | 'module'): void {
+  if (!(tsLoaderMarker in globalThis)) {
+    throw new Error('TypeScript Loader not available')
+  }
+  debug(`Forcing type to "${type}"`)
+  ;(globalThis as any)[tsLoaderMarker] = type
+}
+
+/* ========================================================================== *
+ * MAIN ENTRY POINT                                                           *
+ * ========================================================================== */
+export function main(callback: (args: string[]) => void): void {
+  /* Check for source maps and typescript support */
+  const sourceMapsEnabled = process.execArgv.indexOf('--enable-source-maps') >= 0
+
+  /* Check if our `ts-loader` loader is enabled */
+  const tsLoaderMarker = Symbol.for('plugjs:tsLoader')
+  const typeScriptEnabled = (globalThis as any)[tsLoaderMarker]
+
+  /* Some debugging if needed */
+  debug('SourceMaps enabled =', sourceMapsEnabled)
+  debug('TypeScript enabled =', typeScriptEnabled || false)
+
+  /* If both source maps and typescript are on, run! */
+  if (sourceMapsEnabled && typeScriptEnabled) {
+    const args = process.argv.slice(2).filter((arg: string) => {
+      if (arg === '--force-esm') {
+        forceType('module')
+      } else if (arg === '--force-cjs') {
+        forceType('commonjs')
+      } else {
+        return arg
+      }
+    })
+
+
+    return callback(args)
+  } else {
+    const script = _url.fileURLToPath(import.meta.url)
+
+    /* Fork out ourselves with new options */
+    const execArgv = [ ...process.execArgv ]
+
+    /* Enable source maps if not done already */
+    if (! sourceMapsEnabled) execArgv.push('--enable-source-maps')
+
+    /* Enable our ESM TypeScript loader if not done already */
+    if (! typeScriptEnabled) {
+      const directory = _path.dirname(script)
+      const extension = _path.extname(script) // .mts or .mjs
+      const loader = _path.resolve(directory, `ts-loader${extension}`)
+      execArgv.push(`--experimental-loader=${loader}`, '--no-warnings')
+    }
+
+    /*
+     * It seems that setting "type" as "module" in "package.json" creates some
+     * problems when the module is being imported from a "commonjs" one.
+     *
+     * TypeScript _incorrectly_ says (regardless of how we set up our conditional
+     * exports) that we must use dynamic imports:
+     *
+     *     Module '@plugjs/plug' cannot be imported using this construct. The
+     *     specifier only resolves to an ES module, which cannot be imported
+     *     synchronously. Use dynamic import instead.
+     *     TS(1471)
+     *
+     * So for now our only option is to leave "type" as "commonjs", and for those
+     * brave souls willing to force ESM irregardless of what's in "package.json",
+     * we allow the "--force-esm" option, and instruct `ts-loader` that the
+     * current directory (and subdirs) will transpile as ESM always.
+     */
+
+    /* Fork ourselves! */
+    const child = _childProcess.fork(script, [ ...process.argv.slice(2) ], {
+      stdio: [ 'inherit', 'inherit', 'inherit', 'ipc' ],
+      execArgv,
+    })
+
+    /* Monitor child process... */
+    child.on('error', (error) => {
+      console.log('Error respawning CLI', error)
+      process.exit(1)
+    })
+
+    child.on('exit', (code, signal) => {
+      if (signal) {
+        console.log(`CLI process exited with signal ${signal}`)
+        process.exit(1)
+      } else if (typeof code !== 'number') {
+        console.log('CLI process failed for an unknown reason')
+        process.exit(1)
+      } else {
+        process.exit(code)
+      }
+    })
+  }
+}
