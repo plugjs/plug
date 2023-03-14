0b6b0443440dc098eb2cc0ef4c65b7d1269f8613
Pier Fumagalli <pier@usrz.com>
2023-03-14 17:52:56 +0100

    Tests for showing differences

---
+++ workspaces/jasmine/test/expectations.test.ts
@@ -0,0 +1,15 @@
+describe('Expectations tests', () => {
+  it('should produce some nice differences for objects', () => {
+    expect({ foo: 'bar', hello: 'world', zap: true })
+        .toEqual({ foo: 'baz', hello: 'world', zap: false })
+  })
+
+  it('should produce some nice differences for arrays', () => {
+    expect([ 'foo', 'bar', 'baz' ]).toEqual([ 'baz', 'bar', 'foo' ])
+  })
+
+  it('should produce some nice differences for strings', () => {
+    expect('The brown fox jumped over the lazy dog')
+        .toEqual('The quick fox jumped over the sleeping dog')
+  })
+})
