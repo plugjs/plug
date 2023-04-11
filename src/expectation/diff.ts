d412b88a263a7efafa34b0fa365c2500b871e2ca
Pier Fumagalli <pier@usrz.com>
2023-04-11 20:25:06 +0200

    Values diff (base for `toEqual(...)`)

---
+++ src/expectation/diff.ts
@@ -0,0 +1,428 @@
+/* eslint-disable no-fallthrough */
+import { stringifyConstructor, stringifyValue } from './types'
+
+import type { Constructor } from './types'
+
+export interface NoDiff {
+  diff: false,
+  actual: string
+}
+
+export interface ValueDiff {
+  diff: true,
+  actual: string,
+  expected: string,
+}
+
+export interface ErrorDiff {
+  diff: true,
+  actual: string,
+  error: string,
+}
+
+export interface ObjectDiff {
+  diff: boolean,
+  actual: string,
+  props?: Record<string, Diff>,
+}
+
+export interface ListDiff extends ObjectDiff {
+  values: Diff[],
+}
+
+export interface MappingsDiff extends ObjectDiff {
+  mappings: [ string, Diff ][]
+}
+
+export type Diff =
+  | NoDiff
+  | ValueDiff
+  | ObjectDiff
+  | ListDiff
+  | ErrorDiff
+
+/* ========================================================================== *
+ * IMPLEMENTATION INTERNALS                                                   *
+ * ========================================================================== */
+
+type Binary = Buffer | Uint8Array | ArrayBuffer | SharedArrayBuffer
+type BoxedPrimitive = Boolean | String | Number
+type Remarks = { __brand_remarks: never }
+
+/* ========================================================================== */
+
+function objectDiff<T extends Record<string, any>>(
+    actual: T,
+    expected: T,
+    remarks: Remarks,
+    keys?: Set<string>,
+): ObjectDiff {
+  // default keys: all keys from both actual and expected objects
+  if (! keys) keys = new Set([ ...Object.keys(actual), ...Object.keys(expected) ])
+  if (! keys.size) return { diff: false, actual: stringifyValue(actual) }
+
+  // evaluate differences between objects
+  let diff = false
+  const props: Record<string, Diff> = {}
+  for (const k of keys) {
+    const result = props[k] = diffValues(actual[k], expected[k], remarks)
+    diff = diff || result.diff
+  }
+
+  // return our differences
+  return { diff, actual: stringifyValue(actual), props }
+}
+
+/* ========================================================================== */
+
+function arrayDiff<T extends Record<number, any> & { length: number }>(
+    actual: T,
+    expected: T,
+    remarks: Remarks,
+): ListDiff | ErrorDiff {
+  // make sure that the length of both arrays is the same
+  if (actual.length !== expected.length) {
+    return {
+      diff: true,
+      actual: stringifyValue(actual),
+      error: `to have length ${expected.length} (length=${actual.length})`,
+    }
+  }
+
+  // prepare a set with _all_ keys from both expected and actual object
+  const keys = new Set([ ...Object.keys(expected), ...Object.keys(actual) ])
+
+  // iterate through the array, checking equality for each item ad the given
+  // index, and _removing_ the index from the set of keys to further analyse
+  let valuesDiff = false
+  const values = new Array<Diff>(expected.length)
+  for (let i = 0; i < expected.length; i ++) {
+    const result = values[i] = diffValues(actual[i], expected[i], remarks)
+    valuesDiff = valuesDiff || result.diff
+    keys.delete(String(i))
+  }
+
+  // analyse the differences between leftover props (if any)
+  const result = objectDiff(actual, expected, remarks, keys)
+  const diff = result.diff || valuesDiff
+
+  return { ...result, diff, values } satisfies ListDiff
+}
+
+/* ========================================================================== */
+
+function setDiff<T>(
+    actual: Set<T>,
+    expected: Set<T>,
+    remarks: Remarks,
+): ListDiff | ErrorDiff {
+  // make sure that the size of both sets is the same
+  if (actual.size !== expected.size) {
+    return {
+      diff: true,
+      actual: stringifyValue(actual),
+      error: `to have size ${expected.size} (size=${actual.size})`,
+    }
+  }
+
+  // check differences between sets
+  const values: Diff[] = []
+  const missing = new Set<T>(expected)
+  const extra = new Set<T>(actual)
+
+  for (const act of extra) {
+    for (const exp of missing) {
+      const diff = diffValues(act, exp, remarks)
+      if (diff.diff) continue
+
+      values.push(diff)
+      extra.delete(act)
+      missing.delete(exp)
+      break
+    }
+  }
+
+  // compare sets as "objects" (for extra properties)
+  const obj = objectDiff(actual, expected, remarks)
+  const diff = !! (missing.size || extra.size || obj.diff)
+  const undef = stringifyValue(undefined)
+
+  // inject all extra and missing properties
+  extra.forEach((value) => values.push({
+    diff: true,
+    actual: stringifyValue(value),
+    expected: undef,
+  }))
+
+  missing.forEach((value) => values.push({
+    diff: true,
+    actual: undef,
+    expected: stringifyValue(value),
+  }))
+
+  // done...
+  return { ...obj, diff, values }
+}
+
+/* ========================================================================== */
+
+function mapDiff<K, V>(
+    actual: Map<K, V>,
+    expected: Map<K, V>,
+    remarks: Remarks,
+): MappingsDiff | ErrorDiff {
+  // make sure that the size of both maps is the same
+  if (actual.size !== expected.size) {
+    return {
+      diff: true,
+      actual: stringifyValue(actual),
+      error: `to have size ${expected.size} (size=${actual.size})`,
+    }
+  }
+
+  // check mappings
+  let diff = false
+  const mappings: [ string, Diff ][] = []
+  for (const key of new Set([ ...actual.keys(), ...expected.keys() ])) {
+    const act = actual.get(key)
+    const exp = expected.get(key)
+    const result = diffValues(act, exp, remarks)
+    mappings.push([ stringifyValue(key), result ])
+    diff = diff || result.diff
+  }
+
+  // check other properties
+  const object = objectDiff(actual, expected, remarks)
+  diff = diff || object.diff
+
+  // return our combined diff
+  return { ...object, diff, mappings }
+}
+
+/* ========================================================================== */
+
+function binaryDiff<T extends Binary>(
+    actual: T,
+    expected: T,
+    actualData: Buffer,
+    expectedData: Buffer,
+    remarks: Remarks,
+): ObjectDiff | ErrorDiff {
+  // make sure that the length of both arrays is the same
+  if (actualData.length !== expectedData.length) {
+    return {
+      diff: true,
+      actual: stringifyValue(actual),
+      error: `to have length ${expectedData.length} (length=${actualData.length})`,
+    }
+  }
+
+  // remember keys
+  const keys = new Set([ ...Object.keys(expected), ...Object.keys(actual) ])
+
+  // check for equality
+  const length = expectedData.length
+  for (let i = 0; i < length; i ++) {
+    keys.delete(String(i))
+    if (actualData[i] === expectedData[i]) continue
+
+    let act = actualData.toString('hex', i, i + 6)
+    let exp = expectedData.toString('hex', i, i + 6)
+    if (act.length > 10) {
+      act = act.substring(0, 10) + '\u2026'
+      exp = exp.substring(0, 10) + '\u2026'
+    }
+    if (i > 0) {
+      act = '\u2026' + act
+      exp = '\u2026' + exp
+    }
+
+    return {
+      diff: true,
+      actual: stringifyValue(actual),
+      error: `to equal at index ${i} (actual=${act}, expected=${exp})`,
+    }
+  }
+
+  // same contents, check extra properties
+  return objectDiff(actual, expected, remarks, keys)
+}
+
+/* ========================================================================== */
+
+function primitiveDiff<T extends BoxedPrimitive>(
+    actual: T,
+    expected: T,
+    remarks: Remarks,
+): ObjectDiff | ValueDiff {
+  if (actual.valueOf() !== expected.valueOf()) {
+    return {
+      diff: true,
+      actual: stringifyValue(actual),
+      expected: stringifyValue(expected),
+    }
+  }
+
+  // remove string indexes from properties
+  const keys = new Set([ ...Object.keys(actual), ...Object.keys(expected) ])
+  if (actual instanceof String) {
+    const value = actual.valueOf()
+    for (let i = 0; i < value.length; i ++) {
+      keys.delete(String(i))
+    }
+  }
+
+  // return either an object diff or no diff at all...
+  return keys.size ? objectDiff(actual, expected, remarks, keys) : {
+    diff: false,
+    actual: stringifyValue(actual),
+  }
+}
+
+/* ========================================================================== *
+ * MAIN "DIFF"                                                                *
+ * ========================================================================== */
+
+function diffValues(actual: any, expected: any, remarks: Remarks): Diff {
+  // strict equality
+  if (expected === actual) return { diff: false, actual: stringifyValue(expected) }
+
+  // oh, javascript!
+  if (expected === null) {
+    return {
+      diff: true,
+      actual: stringifyValue(actual),
+      expected: stringifyValue(null),
+    }
+  }
+
+  // inspect types of what to compare
+  const actualType = typeof actual
+  const expectedType = typeof expected
+
+  // if type is different then highlight the difference
+  if (actualType !== expectedType) {
+    return {
+      diff: true,
+      actual: stringifyValue(actual),
+      expected: stringifyValue(expected),
+    }
+  }
+
+  // primitives
+  switch (actualType) {
+    // numbers are one-of-a-kind as NaN !== NaN
+    case 'number':
+      if (isNaN(expected as number) && isNaN(actual as number)) {
+        return { diff: false, actual: stringifyValue(NaN) }
+      }
+    // primitives always must be strict ===
+    case 'bigint':
+    case 'boolean':
+    case 'function':
+    case 'string':
+    case 'symbol':
+    case 'undefined':
+      return {
+        diff: true,
+        actual: stringifyValue(actual),
+        expected: stringifyValue(expected),
+      }
+    // everything else is an object and must be checked
+  }
+
+  // TODO: matchers!!!
+
+  // check that actual is _assignable_ from expected
+  const prototype = Object.getPrototypeOf(expected)
+  if (prototype && prototype.constructor) {
+    if (! (actual instanceof prototype.constructor)) {
+      return {
+        diff: true,
+        actual: stringifyValue(actual),
+        error: `to be instance of ${stringifyConstructor(prototype.constructor)}`,
+      }
+    }
+  }
+
+  /* == ARRAYS ============================================================== */
+
+  const checkInstance = <T>(
+    ctor: Constructor<T>,
+    callback: (actual: T, expected: T, remarks: Remarks) => Diff,
+  ): Diff | undefined =>
+    (expected instanceof ctor) ?
+      callback(actual as InstanceType<typeof ctor>, expected, remarks) :
+    (actual instanceof ctor) ? {
+      diff: true,
+      actual: stringifyValue(actual),
+      error: `not to be an instance of ${stringifyConstructor(ctor)}`,
+    } : undefined
+
+  return (
+    /* == ARRAYS ============================================================ */
+    checkInstance(Array, arrayDiff) ||
+
+    /* == SETS ============================================================== */
+    checkInstance(Set, (act, exp) => setDiff(act, exp, remarks)) ||
+
+    /* == MAPS ============================================================== */
+    checkInstance(Map, (act, exp) => mapDiff(act, exp, remarks)) ||
+
+    /* == BOXED PRIMITIVES ================================================== */
+    checkInstance(Boolean, primitiveDiff) ||
+    checkInstance(String, primitiveDiff) ||
+    checkInstance(Number, primitiveDiff) ||
+
+    /* == PROMISES (always error, must be ===) ============================== */
+    checkInstance(Promise, (act, exp) => ({
+      diff: true,
+      actual: stringifyValue(act),
+      error: `to strictly equal ${stringifyValue(exp)}`,
+    })) ||
+
+    /* == DATES ============================================================= */
+    checkInstance(Date, (act, exp) =>
+      (act.getTime() !== exp.getTime()) ? {
+        diff: true,
+        actual: stringifyValue(act),
+        expected: stringifyValue(exp),
+      } : objectDiff(act, exp, remarks)) ||
+
+    /* == REGULAR EXPRESSIONS =============================================== */
+    checkInstance(RegExp, (act, exp) =>
+      ((act.source !== exp.source) || (act.flags !== exp.flags)) ? {
+        diff: true,
+        actual: stringifyValue(act),
+        expected: stringifyValue(exp),
+      } : objectDiff(act, exp, remarks)) ||
+
+    /* == BINARY ARRAYS ===================================================== */
+    checkInstance(Buffer, (act, exp) => binaryDiff(act, exp, act, exp, remarks)) ||
+    checkInstance(Uint8Array, (act, exp) => binaryDiff(act, exp, Buffer.from(act), Buffer.from(exp), remarks)) ||
+    checkInstance(ArrayBuffer, (act, exp) => binaryDiff(act, exp, Buffer.from(act), Buffer.from(exp), remarks)) ||
+    checkInstance(SharedArrayBuffer, (act, exp) => binaryDiff(act, exp, Buffer.from(act), Buffer.from(exp), remarks)) ||
+
+    /* == OTHER TYPED ARRAYS ================================================ */
+    checkInstance(BigInt64Array, arrayDiff) ||
+    checkInstance(BigUint64Array, arrayDiff) ||
+    checkInstance(Float32Array, arrayDiff) ||
+    checkInstance(Float64Array, arrayDiff) ||
+    checkInstance(Int16Array, arrayDiff) ||
+    checkInstance(Int32Array, arrayDiff) ||
+    checkInstance(Int8Array, arrayDiff) ||
+    checkInstance(Uint16Array, arrayDiff) ||
+    checkInstance(Uint32Array, arrayDiff) ||
+    checkInstance(Uint8ClampedArray, arrayDiff) ||
+
+    /* == DONE ============================================================== */
+    objectDiff(actual as any, expected, remarks))
+}
+
+/* ========================================================================== *
+ * EXPORTS                                                                    *
+ * ========================================================================== */
+
+export function diff(actual: any, expected: any): Diff {
+  return diffValues(actual, expected, {} as Remarks)
+}
