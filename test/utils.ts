16e935f74fc58c310d922205985d356ae87b5638
Pier Fumagalli <pier@usrz.com>
2023-04-12 18:54:42 +0200

    Better errors

--- test/utils.ts
+++ test/utils.ts
@@ -5,14 +5,19 @@ import { ExpectationError } from '../src/expectation/types'
 import type { Diff } from '../src/expectation/diff'
 
 export function expectPass(expectation: () => void): void {
-  assert.doesNotThrow(expectation)
+  try {
+    expectation()
+    return
+  } catch (error) {
+    assert.strictEqual(error, undefined)
+  }
 }
 
 export function expectFail(expectation: () => void, message: string, diff?: Diff): void {
   assert.throws(expectation, (thrown) => {
     assert(thrown instanceof ExpectationError, 'Error type')
-    assert.strictEqual(thrown.message, message, 'Error message')
-    if (diff) assert.deepEqual(thrown.diff, diff, 'Error diff mismatch')
+    assert.strictEqual(thrown.message, message)
+    if (diff) assert.deepEqual(thrown.diff, diff)
     if ((! diff) && thrown.diff) assert.deepEqual(thrown.diff, diff, 'Error diff missing')
     return true
   })
