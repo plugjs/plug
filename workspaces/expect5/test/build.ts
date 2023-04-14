a25594403ab7c04c57569c50b10a30edf18fa48e
Pier Fumagalli <pier@usrz.com>
2023-04-14 18:14:52 +0200

    Long task names

--- workspaces/expect5/test/build.ts
+++ workspaces/expect5/test/build.ts
@@ -3,21 +3,20 @@ import { assert, build, find, merge } from '@plugjs/plug'
 import { Test } from '../src/test'
 
 export default build({
-  async test_self() {
+  async ['self expect5 test']() {
     await find('*.test.ts', { directory: '@' }).plug(new Test())
   },
 
-  async test_install() {
+  async ['install expect5 test']() {
     const pipe1 = merge([])
     assert(typeof pipe1.test === 'undefined', 'Expect5 already installed')
-    // @ts-ignore
     await import('../src/index')
     const pipe2 = merge([])
     assert(typeof pipe2.test === 'function', 'Expect5 not installed')
   },
 
   async test(): Promise<void> {
-    await this.test_self()
-    await this.test_install()
+    await this['self expect5 test']()
+    await this['install expect5 test']()
   },
 })
