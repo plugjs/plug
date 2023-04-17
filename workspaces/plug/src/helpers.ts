8c36a1894c658ee7da031e7e99a0d761b8c731b3
Pier Fumagalli <pier@usrz.com>
2023-04-17 13:09:28 +0200

    New `invokeBuild` helper, to invoke sub builds

--- workspaces/plug/src/helpers.ts
+++ workspaces/plug/src/helpers.ts
@@ -7,15 +7,23 @@ import { requireContext } from './async'
 import { Files } from './files'
 import { rm } from './fs'
 import { $p, log } from './logging'
-import { commonPath, getCurrentWorkingDirectory, resolveDirectory, resolveFile } from './paths'
+import {
+  commonPath,
+  getAbsoluteParent,
+  getCurrentWorkingDirectory,
+  resolveDirectory,
+  resolveFile,
+} from './paths'
 import { PipeImpl } from './pipe'
+import { RunBuild } from './plugs/build'
 import { execChild } from './utils/exec'
 import { parseOptions } from './utils/options'
 import { walk } from './utils/walk'
 
-import type { Context } from './pipe'
+import type { ForkOptions } from './fork'
 import type { Pipe } from './index'
 import type { AbsolutePath } from './paths'
+import type { Context } from './pipe'
 import type { ExecChildOptions } from './utils/exec'
 import type { ParseOptions } from './utils/options'
 import type { WalkOptions } from './utils/walk'
@@ -60,6 +68,44 @@ export function find(...args: ParseOptions<FindOptions>): Pipe {
 
     return builder.build()
   }))
+}
+
+export type InvokeBuildOptions = ForkOptions & Record<string, string>
+export type InvokeBuildTasks = string | [ string, ...string[] ]
+
+export function invokeBuild(buildFile: string): Promise<void>
+export function invokeBuild(buildFile: string, task: string): Promise<void>
+export function invokeBuild(buildFile: string, task: string, options: InvokeBuildOptions): Promise<void>
+export function invokeBuild(buildFile: string, tasks: [ string, ...string[] ]): Promise<void>
+export function invokeBuild(buildFile: string, tasks: [ string, ...string[] ], options: InvokeBuildOptions): Promise<void>
+export function invokeBuild(buildFile: string, options: InvokeBuildOptions): Promise<void>
+export async function invokeBuild(
+    buildFile: string,
+    tasksOrOptions?: string | [ string, ...string[] ] | InvokeBuildOptions,
+    maybeOptions?: InvokeBuildOptions,
+): Promise<void> {
+  const [ tasks, options = {} ] =
+      typeof tasksOrOptions === 'string' ?
+          [ [ tasksOrOptions ], maybeOptions ] :
+      Array.isArray(tasksOrOptions) ?
+          [ tasksOrOptions, maybeOptions ] :
+      typeof tasksOrOptions === 'object' ?
+          [ [ 'default' ], tasksOrOptions ] :
+      [ [ 'default' ], {} ]
+
+  if (tasks.length === 0) tasks.push('default')
+
+  const { coverageDir, forceModule, ...props } = options
+  const forkOptions = { coverageDir, forceModule }
+
+  const context = requireContext()
+  const file = context.resolve(buildFile)
+  const dir = getAbsoluteParent(file)
+  const files = Files.builder(dir).add(file).build()
+
+  return new RunBuild(tasks, props, forkOptions)
+      .pipe(files, context)
+      .then(() => void 0)
 }
 
 /**
