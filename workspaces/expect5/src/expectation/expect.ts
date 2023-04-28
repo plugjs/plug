b308c34eb721909e2786d65919e803f5d1d2786e
Pier Fumagalli <pier@usrz.com>
2023-04-28 16:35:50 +0200

    Cleanup

--- workspaces/expect5/src/expectation/expect.ts
+++ workspaces/expect5/src/expectation/expect.ts
@@ -1,13 +1,16 @@
-import { AsyncExpectations } from './expectations'
+import { AsyncExpectations } from './async'
 import { Matchers } from './matchers'
 
-export { Expectations } from './expectations'
+export type { AsyncExpectations } from './async'
+export type { Expectations, NegativeExpectations } from './expectations'
+export type { Matchers, NegativeMatchers } from './matchers'
 
 /* ========================================================================== *
  * EXPECT FUNCTION                                                            *
  * ========================================================================== */
 
-type Expect = {
+/** The `expect` function exposing expectations and matchers */
+export type Expect = {
   <T = unknown>(value: T, remarks?: string): AsyncExpectations<T>
 } & Omit<Matchers, 'expect'>
 
@@ -16,6 +19,7 @@ export const expect: Expect = ((value: any, remarks?: string) => {
   return new AsyncExpectations(value, remarks)
 }) as Expect
 
+/* Inject all our matchers constructors in the `expect` function */
 for (const key of Object.getOwnPropertyNames(Matchers.prototype)) {
   if (! key.startsWith('to')) continue
 
@@ -24,4 +28,5 @@ for (const key of Object.getOwnPropertyNames(Matchers.prototype)) {
   Object.defineProperty(expect, key, { value: matcher })
 }
 
+/* Inject the negative matcher constructor in the `expect` function */
 Object.defineProperty(expect, 'not', { get: () => new Matchers().not })
