94c012efdb00acb2f94c296779a21d46a07d284c
Pier Fumagalli <pier@usrz.com>
2023-05-04 01:39:00 +0200

    TSD tests are *SLOW*

--- workspaces/tsd/test/test.ts
+++ workspaces/tsd/test/test.ts
@@ -8,23 +8,23 @@ describe('Tsd Plug', () => {
   it('should succeed with only passing tests', async () => {
     await find('**/passing.test-d.ts', { directory: dataDir })
         .plug(new Tsd({ cwd: dataDir }))
-  })
+  }, 10_000)
 
   it('should fail including failing tests', async () => {
     await expect(find('**/*.test-d.ts', { directory: dataDir })
         .plug(new Tsd({ cwd: dataDir, typingsFile: `${dataDir}/index.d.ts` })))
         .toBeRejectedWithError(BuildFailure)
-  })
+  }, 10_000)
 
   it('should fail with an incorrect typings file', async () => {
     await expect(find('**/*.test-d.ts', { directory: dataDir })
         .plug(new Tsd({ cwd: dataDir, typingsFile: `${dataDir}/does-not-exist.d.ts` })))
         .toBeRejectedWithError(Error, /The type definition `does-not-exist.d.ts` does not exist/)
-  })
+  }, 10_000)
 
   it('should install the "tsd" plug', async () => {
     expect(merge([]).tsd).toBeUndefined()
     await import('../src/index')
     expect(merge([]).tsd).toBeA('function')
-  })
+  }, 10_000)
 })
