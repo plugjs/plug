aad3201f1b44f8638aa539509550cd5b6580857a
Pier Fumagalli <pier@usrz.com>
2023-05-10 14:59:14 +0200

    Export some global types

---
+++ workspaces/plug/src/globals.ts
@@ -0,0 +1,3 @@
+// Re-refence our global exports. This is exported "as is" so that we can
+// more simply add "@plugjs/plug/globals" in "tsconfig.json"'s own "types"
+/// <reference path="../types/globals.d.ts" />
