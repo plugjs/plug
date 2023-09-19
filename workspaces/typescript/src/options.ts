ffc1043fb635a39042617245f907d783af1e3ef8
Pier Fumagalli <pier@usrz.com>
2023-09-19 11:45:06 +0200

    Cleanup and export project references

--- workspaces/typescript/src/options.ts
+++ workspaces/typescript/src/options.ts
@@ -8,70 +8,9 @@ import type { AbsolutePath } from '@plugjs/plug/paths'
 export type CompilerOptionsAndDiagnostics = {
   options: ts.CompilerOptions,
   errors: readonly ts.Diagnostic[],
+  projectReferences?: readonly ts.ProjectReference[];
 }
 
-/* ========================================================================== */
-
-// function mergeResults(
-//     base: CompilerOptionsAndDiagnostics,
-//     override: CompilerOptionsAndDiagnostics,
-// ): CompilerOptionsAndDiagnostics {
-//   const options = { ...base.options, ...override.options }
-//   const errors = [ ...base.errors, ...override.errors ]
-//   return errors.length ? { options: {}, errors } : { options, errors: [] }
-// }
-
-/* ========================================================================== */
-
-// async function loadOptions(
-//     file: AbsolutePath,
-//     stack: AbsolutePath[] = [ file ],
-// ): Promise<CompilerOptionsAndDiagnostics> {
-//   const dir = getAbsoluteParent(file)
-
-//   // Load up our config file and convert is wicked JSON
-//   const { config, error } = ts.readConfigFile(file, ts.sys.readFile)
-//   if (error) return { options: {}, errors: [ error ] }
-
-//   // Parse up the configuration file as options
-//   const { compilerOptions = {}, extends: extendsPath } = config
-//   const result = ts.convertCompilerOptionsFromJson(compilerOptions, dir, file)
-//   if (result.errors.length) return result
-
-//   // If we don't extend, we can return our result
-//   if (!extendsPath) return result
-
-//   // Resolve the name of the file this config extends
-//   const ext = resolveAbsolutePath(dir, extendsPath)
-
-//   // Triple check that we are not recursively importing this file
-//   if (stack.includes(ext)) {
-//     const data = ts.sys.readFile(file)
-//     return { options: {}, errors: [ {
-//       messageText: `Circularity detected extending from "${ext}"`,
-//       category: ts.DiagnosticCategory.Error,
-//       code: 18000, // copied from typescript internals...
-//       file: ts.createSourceFile(file, data!, ts.ScriptTarget.JSON, false, ts.ScriptKind.JSON),
-//       start: undefined,
-//       length: undefined,
-//     } ] }
-//   }
-
-//   // Push our file in the stack and load recursively
-//   return mergeResults(await loadOptions(ext, [ ...stack, ext ]), result)
-// }
-
-/* ========================================================================== */
-
-// export async function getCompilerOptions(
-//   file?: AbsolutePath,
-// ): Promise<CompilerOptionsAndDiagnostics>
-
-// export async function getCompilerOptions(
-//   file: AbsolutePath | undefined,
-//   overrides: ts.CompilerOptions,
-//   inputs: AbsolutePath[],
-// ): Promise<CompilerOptionsAndDiagnostics>
 
 /** Load compiler options from a JSON file, and merge in the overrides */
 export async function getCompilerOptions(
