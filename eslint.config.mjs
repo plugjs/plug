665204abe0c9b56dfb098c31b9106204a1f02ecc
Pier Fumagalli <pier@usrz.com>
2024-11-19 17:42:47 +0100

    Add ignores for when we run `eslint` manually

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -41,4 +41,18 @@ export default [
       } ],
     },
   },
+
+  // ===== IGNORED FILES =======================================================
+  // REMEMBER! Ignores *must* be in its own configuration, they can not coexist
+  // with "rules", "languageOptions", "files", ... or anything else, otherwise
+  // ESLint will blaantly ignore the ignore files!
+  {
+    ignores: [
+      'coverage/',
+      'node_modules/',
+      'workspaces/*/dist/',
+      'workspaces/cov8/test/sources/',
+      'workspaces/eslint/test/data/',
+    ],
+  },
 ]
