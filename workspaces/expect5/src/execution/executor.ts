d9fb3ac128fc3c1c2f650d145c0456e1bf8c6aa4
Pier Fumagalli <pier@usrz.com>
2025-07-14 15:42:35 +0200

    Record results for a summary report

--- workspaces/expect5/src/execution/executor.ts
+++ workspaces/expect5/src/execution/executor.ts
@@ -21,7 +21,7 @@ export interface ExecutionEvents {
   'spec:start': (spec: Spec) => void
   'spec:error': (spec: Spec, failure: ExecutionFailure) => void
   'spec:skip': (spec: Spec, time: number) => void
-  'spec:pass': (spec: Spec, time: number) => void
+  'spec:pass': (spec: Spec, time: number, slow: boolean) => void
   'spec:fail': (spec: Spec, time: number, failure: ExecutionFailure) => void
 
   'hook:start': (hook: Hook) => void
@@ -30,6 +30,30 @@ export interface ExecutionEvents {
   'hook:pass': (hook: Hook, time: number) => void
   'hook:fail': (hook: Hook, time: number, failure: ExecutionFailure) => void
 }
+
+interface SuiteRecord {
+  type: 'suite'
+  name: string
+  records: (SuiteRecord | SpecRecord | HookRecord)[]
+}
+
+interface SpecRecord {
+  type: 'spec'
+  name: string
+  result: 'passed' | 'failed' | 'skipped'
+  failure?: number
+  slow?: boolean
+  ms: number
+}
+
+interface HookRecord {
+  type: 'hook'
+  name: string
+  result: 'failed'
+  failure: number
+}
+
+export type Record = SuiteRecord | SpecRecord | HookRecord
 
 export interface Execution {
   on<E extends keyof ExecutionEvents>(event: E, listener: ExecutionEvents[E]): this
@@ -43,7 +67,8 @@ export interface ExecutionResult {
   failed: number
   skipped: number
   time: number
-  failures: ExecutionFailure[],
+  failures: ExecutionFailure[]
+  records: Record[]
 }
 
 /* ========================================================================== */
@@ -64,6 +89,7 @@ export function runSuite(suite: Suite): Execution {
     failed: 0,
     skipped: 0,
     failures: [],
+    records: [],
   }
 
   const execution: Execution = {
@@ -113,7 +139,8 @@ export function runSuite(suite: Suite): Execution {
           _emitter.emit(`${type}:skip`, executable, time)
           if (type === 'spec') result.skipped ++
         } else {
-          _emitter.emit(`${type}:pass`, executable, time)
+          const slow = time >= (executable.timeout * 0.75)
+          _emitter.emit(`${type}:pass`, executable, time, slow)
           if (type === 'spec') result.passed ++
         }
       },
@@ -132,12 +159,48 @@ export function runSuite(suite: Suite): Execution {
     }
   }
 
+  // Record of all our suites / specs / hooks
+  const suiteRecords: SuiteRecord[] = [ { type: 'suite', name: '', records: [] } ]
+
+  execution.on('suite:start', (current) => {
+    const record: SuiteRecord = { type: 'suite', name: current.name, records: [] }
+    suiteRecords[suiteRecords.length - 1]!.records.push(record)
+    suiteRecords.push(record)
+  })
+
+  execution.on('suite:done', () => {
+    if (suiteRecords.length > 1) suiteRecords.pop()
+  })
+
+  execution.on('spec:skip', (spec, ms) => {
+    const record: SpecRecord = { type: 'spec', name: spec.name, result: 'skipped', ms }
+    suiteRecords[suiteRecords.length - 1]!.records.push(record)
+  })
+
+  execution.on('spec:pass', (spec, ms) => {
+    const record: SpecRecord = { type: 'spec', name: spec.name, result: 'passed', ms }
+    suiteRecords[suiteRecords.length - 1]!.records.push(record)
+    if (ms >= (spec.timeout * 0.75)) record.slow = true
+  })
+
+  execution.on('spec:fail', (spec, ms, { number }) => {
+    const record: SpecRecord = { type: 'spec', name: spec.name, result: 'failed', failure: number, ms }
+    suiteRecords[suiteRecords.length - 1]!.records.push(record)
+  })
+
+  execution.on('hook:fail', (hook, _, { number }) => {
+    const record: HookRecord = { type: 'hook', name: hook.name, result: 'failed', failure: number }
+    suiteRecords[suiteRecords.length - 1]!.records.push(record)
+  })
+
+  // Start the execution
   setImmediate(() => Promise.resolve().then(async () => {
     const now = Date.now()
     await suite.setup()
     await suite.execute({ start })
 
     result.time = Date.now() - now
+    result.records.push(...suiteRecords[0]!.records)
 
     resolve(result)
   }).catch((error) => reject(error)))
