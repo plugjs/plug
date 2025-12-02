954ac9eb2bc9184911caa11cf21eb0e1794ef614
Pier Fumagalli <pier@usrz.com>
2025-12-02 23:35:35 +0100

    Make sure `tscBuild` is installed

--- workspaces/typescript/test/99-install.test.ts
+++ workspaces/typescript/test/99-install.test.ts
@@ -1,9 +1,11 @@
 import { merge } from '@plugjs/plug'
 
 describe('TypeScript Plug installation', () => {
-  it('should install the "tsc" plug', async () => {
+  it('should install the "tsc" and "tscBuild" plugs', async () => {
     expect(merge([]).tsc).toBeUndefined()
+    expect(merge([]).tscBuild).toBeUndefined()
     await import('../src/index')
     expect(merge([]).tsc).toBeA('function')
+    expect(merge([]).tscBuild).toBeA('function')
   })
 })
