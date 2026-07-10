2712bcf6e2ecac55ae018c8f8c5fb8333da958db
Pier Fumagalli <pier@usrz.com>
2026-07-11 02:00:27 +0900

    Test `tsc`

--- workspaces/typescript/test/04-compiler.test.ts
+++ workspaces/typescript/test/04-compiler.test.ts
@@ -1,6 +1,6 @@
 import { find, mkdtemp, rmrf } from '@plugjs/plug'
 
-import { TscCompiler } from '../src/tsccompiler'
+import { TscCompiler, tsc } from '../src/tsccompiler'
 
 import type { AbsolutePath } from '@plugjs/plug'
 
@@ -31,6 +31,31 @@ describe('TypeScript Compiler', () => {
 
     // Build our project and check the resulting (written) files
     const result = await find('tsconfig.json', { directory: tempDir }).plug(new TscCompiler())
+    expect([ ...result ]).toMatchContents([
+      'dist/foobar.d.ts',
+      'dist/foobar.js',
+      'dist/foobar.js.map',
+    ])
+
+    // Check the full tree *after* building   our target files
+    const targets = await find('**/*', { directory: tempDir })
+    expect([ ...targets ]).toMatchContents([ ...sources, ...result ])
+  })
+
+  it('should build a full project using "tsc"', async () => {
+    // Copy our builder test files to a temp directory
+    const sources = await find('**/*', { directory: `${testDir}/compiler` }).copy(tempDir)
+
+    // Check our source files
+    expect([ ...sources ]).toMatchContents([
+      'src/foobar.ts',
+      'tsconfig.json',
+      'tsconfig.options.json',
+    ])
+
+    // Build our project and check the resulting (written) files
+    const result = await tsc({ directory: tempDir })
+
     expect([ ...result ]).toMatchContents([
       'dist/foobar.d.ts',
       'dist/foobar.js',
